158dca16934566be46e9f45be4af757bdb1b1040
seanseancooper <sean.darwin.cooper@gmail.com>
2024-11-05 17:05:27 -0700

    moved into lib...  not working correctly yet.
    The issue with the approach is that I am not  interpreting JS on either side. I may move this to a worker/SSR at some point later.

--- src/gps/lib/JavaScriptGPSClient.py
+++ src/gps/lib/JavaScriptGPSClient.py
@@ -20,48 +20,25 @@ class JavaScriptGPSClient:
         self.resp = None
 
     def javascript_lines(self):
-        from http import client
-
-        # self.conn = client.HTTPConnection('localhost:5173')
-        # self.conn.set_debuglevel(1)
-
-        headers = {
-            # "Access-Control-Allow-Origin": "localhost:*",
-            # "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-            # "Accept-Encoding": "gzip, deflate, br",
-            # "Accept-Language": "en-US,en;q=0.5",
-            # "Cache-Control": "no-cache",
-            # "Connection": "keep-alive",
-            # "DNT": 1,
-            # "Sec-Fetch-Dest": "document",
-            # "Sec-Fetch-Mode": "navigate",
-            # "Sec-Fetch-Site": "cross-site",
-            # "Upgrade-Insecure-Requests": 1,
-        }
-
-        # self.conn.request("GET", "http://localhost:5173/JavaScriptGPSRetriever.js")
-        # self.resp = self.conn.getresponse()
-        # self.conn.close()
-
         import requests
-        self.resp = requests.get("http://localhost:5173/JavaScriptGPSRetriever.js")
-
+        self.resp = requests.get("http://localhost:5173/JavaScriptGPSRetriever.jsx")
         return self.resp
 
     def json_stream(self):
         for line in self.javascript_lines():
             answ = line.decode('utf-8').strip()
-            print(f'answ: {answ}')
-            if answ.startswith('{"GPS"'):
-                yield json.loads(answ)
+            print(f'json_stream answ: {answ}')
+            if answ.find('//#') > 0:
+                yield answ
             else:
                 self.close()
 
     def dict_stream(self) -> Iterable[Dict[str, Any]]:
         if self.json_stream():
-            for line in self.json_stream():
-                line["GPS"]["UPDATED"] = datetime.now().__format__("%Y-%m-%d %H:%M:%S")
-                yield line
+            for line in self.json_stream(): pass
+                # print(f'dict_stream line: {line}')
+                # line["GPS"]["UPDATED"] = datetime.now().__format__("%Y-%m-%d %H:%M:%S")
+                # yield line
 
     def close(self):
         if self.conn:
