d498e778ef49a6b15d7cf2d082addaac110d1387
seanseancooper <sean.darwin.cooper@gmail.com>
2024-11-10 23:29:35 -0700

    removed unused code; I'm satisfied for now with how sell ans easy this is to work with. It does exactly what it needs to do right now, is easily understood, maintainable and generally easy to use. So ln. 42 doesn't give me any pause  -- not changing this.

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,4 +1,5 @@
 import os
+import json
 import logging.config
 
 CONFIG_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -7,22 +8,16 @@ LOG_PATH = os.path.join(CONFIG_PATH, '../../logs')
 if not os.path.exists(LOG_PATH):
     os.mkdir(LOG_PATH)
 
-logging.config.fileConfig(CONFIG_PATH + "/logging.conf", defaults={"LOG_PATH": LOG_PATH}, disable_existing_loggers=False)
-
+logging.config.fileConfig(CONFIG_PATH + "/logging.conf",
+                          defaults={"LOG_PATH": LOG_PATH},
+                          disable_existing_loggers=False)
 
 def readConfig(config_file, config):
 
     if isinstance(config, dict):
         with open(config_file, "r") as f:
-            import json
             data = json.load(f)
 
             for category, settings in data.items():
-                setting_list = {}
-
                 for setting in settings:
-                    setting_list[list(setting.keys()).pop(0)] = list(setting.values()).pop(0)
-
-                    key = list(setting.keys()).pop(0)
-                    value = list(setting.values()).pop(0)
-                    config[key] = value
+                    config[list(setting.keys()).pop(0)] = list(setting.values()).pop(0)
