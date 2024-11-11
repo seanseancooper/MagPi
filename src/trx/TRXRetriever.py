dcb5f426f0254747784dcd213977ecea7bb7c57a
seanseancooper <sean.darwin.cooper@gmail.com>
2024-11-10 23:33:20 -0700

    Produces JSON TRX scanner output. Serial comms still down!

--- src/trx/TRXRetriever.py
+++ src/trx/TRXRetriever.py
@@ -3,7 +3,6 @@ import threading
 import random
 
 import serial
-import json
 
 import time
 from datetime import datetime
@@ -11,31 +10,6 @@ from datetime import datetime
 from src.config import CONFIG_PATH, readConfig
 
 import logging
-
-# <STX>  An ASCII “Start of Text” symbol (0x02)
-STX = b'0x02'
-
-# <msgCode>  A single character code that represents the command
-# or response message type. Please be aware that this code is case
-# sensitive (i.e. T and t are not the same)A single character code
-# that represents the command or response message type. Please be
-# aware that this code is case sensitive (i.e. T and t are not the same)
-msgCode = b'A'
-
-# <msgData>  The data that accompanies a command or a response.
-# Not all requests require this item. The length and format of
-# this data depends on the type of request being made.
-msgData = b''
-
-# <ETX>  An ASCII “End of Text” symbol (0x03)
-ETX = b'0x03'
-
-# <sum>  An unsigned char type sum of all bytes starting with
-# <msgCode> up to and including <ETX> anded with the value 0xFF.
-# This value must be calculated and sent with every command
-# and response for error checking.
-SUM = ""
-
 
 
 class TRXRetriever(threading.Thread):
@@ -51,11 +25,10 @@ class TRXRetriever(threading.Thread):
         self.bytesize = None
         self.stopbits = None
 
-        self.resp = None
+        self.out = None
 
         self.retrieving = False
         self.thread = None
-        self.result = dict()
 
     def __str__(self):
         return {f"TRXRetriever: "}
@@ -76,37 +49,60 @@ class TRXRetriever(threading.Thread):
         try:
             if self.config['TEST_FILE']:
 
-                lines = [line.strip().replace('"', '').replace('  ', '') for line in open(self.config['TEST_FILE'], 'r')]
-                keys = lines[0].replace('"', '').split(',')
+                lines = [line.strip().replace('"', '').replace('<', '').replace('  ', '') for line in open(self.config['TEST_FILE'], 'r')]
+                keys = lines[0].split(',')
 
                 FIX_TIME = False
 
                 while True:
                     for line in lines[1:-1]:
                         vals = line.split(',')
-                        out = dict([(keys[i], vals[i]) for i in range(len(keys))])
+                        self.out = dict([(keys[i], vals[i]) for i in range(len(keys))])
                         if FIX_TIME:
-                            out['COMP_DATE'] = format(datetime.now(), self.config['DATE_FORMAT'])
-                            out['COMP_TIME'] = format(datetime.now(), self.config['TIME_FORMAT'])
-                            out['SCAN_DATE'] = format(datetime.now(), self.config['DATE_FORMAT'])
-                            out['SCAN_TIME'] = format(datetime.now(), self.config['TIME_FORMAT'])
+                            self.out['COMP_DATE'] = format(datetime.now(), self.config['DATE_FORMAT'])
+                            self.out['COMP_TIME'] = format(datetime.now(), self.config['TIME_FORMAT'])
+                            self.out['SCAN_DATE'] = format(datetime.now(), self.config['DATE_FORMAT'])
+                            self.out['SCAN_TIME'] = format(datetime.now(), self.config['TIME_FORMAT'])
                         time.sleep(random.randint(1, self.config['TEST_FILE_TIME_MAX']))
-                        print(f'{out}')
+                        print(f'{self.out}')
             else:
-                with serial.Serial(self.device, self.rate, parity=self.parity, bytesize=self.bytesize, stopbits=self.stopbits, timeout=1) as ser:
 
-                    # msg = stx + msgCode + etx
-                    # sum = msg >> 0xFF
-                    # resp.write(msg + sum)
+                # <STX>  An ASCII “Start of Text” symbol (0x02)
+                STX = b'\x02'
 
-                    while True:
-                        # self.resp = ser.readline
-                        self.resp = ser.read(10)
-                        print("GOT: " + str(self.resp))
+                # <msgCode>  A single character code that represents the command
+                # or response message type. Please be aware that this code is case
+                # sensitive (i.e. T and t are not the same)
+                msgCode = b'P'
+
+                # <msgData>  The data that accompanies a command or a response.
+                # Not all requests require this item. The length and format of
+                # this data depends on the type of request being made.
+                msgData = b''
+
+                # <ETX>  An ASCII “End of Text” symbol (0x03)
+                ETX = b'\x03'
+
+                # <sum> An unsigned char type sum of all bytes starting with
+                # <msgCode> up to and including <ETX> anded with the value 0xFF.
+                # This value must be calculated and sent with every command
+                # and response for error checking.
+                SUM = sum(msgCode + ETX) and b'\xFF'
+                try:
+
+                    with serial.Serial(self.device, self.rate, parity=self.parity, bytesize=self.bytesize, stopbits=self.stopbits, timeout=1) as ser:
+                        while ser.is_open:
+                            ser.write(STX + msgCode + ETX + bytes(SUM))
+                            self.out = ser.readline()
+                            # self.out = ser.read(10)
+                            print(f'{self.out}')
+
+                except Exception as e:
+                    print(f'{e}')
 
         except Exception as e:
             print(f'{e}')
 
 
 if __name__ == '__main__':
-    TRXRetriever().run()
+    print(TRXRetriever().run())
