3a198af63d2c768d6eb11f56b386c7db07d8aa60
seanseancooper <sean.darwin.cooper@gmail.com>
2024-11-11 22:51:21 -0700

    a little better, but needs to update when modules suddenly go offline.

--- src/map/MAPAggregator.py
+++ src/map/MAPAggregator.py
@@ -19,61 +19,70 @@ def format_time(_, fmt):
 class MAPAggregator(threading.Thread):
     """ New MAPAggregator stub to visit module REST contexts, retrieve data,
     'tag' it, and aggregate to a unified defaultdict for the MAP to consume
-    via single localized module REST context. """
+    via single localized module REST context.
+
+    This will not update the aggregation if the module goes offline!
+
+    """
     def __init__(self):
         super().__init__()
         self.DEBUG = False
         self.config = {}
-        self.modules_config = defaultdict(dict)
+        self.configs = defaultdict(dict)
+
         self.modules = []
         self.live_modules = []
         self.dead_modules = []
+
         self.aggregated = defaultdict(str)
 
-        self.retrieving = False
         self.thread = None
-        self.result = dict()
 
     def __str__(self):
-        return {f"MAPAggregator: "}
+        return {f"MAPAggregator: {self.aggregated}"}
 
     def configure(self, config_file):
-        # read ALL configs; ignore incoming config_file.
-        for module_config in sorted(glob.glob(CONFIG_PATH + "/*.json")):
+        # read ALL configs except controller
+        configs = glob.glob(CONFIG_PATH + "/*.json")
+        configs.remove(os.path.join(CONFIG_PATH, 'controller.json'))
+        configs.remove(os.path.join(CONFIG_PATH, config_file))
+
+        for module_config in configs:
             mod = os.path.basename(module_config).replace('.json', '').upper()
             self.modules.append(mod)
-            readConfig(module_config, self.modules_config[mod])
+            readConfig(module_config, self.configs[mod])
 
-        self.config = self.modules_config['MAP'].copy()
+        readConfig(os.path.join(CONFIG_PATH, config_file), self.config)
 
     def register_modules(self):
-        """ Register module REST contexts """
+        """ Register 'live' module REST contexts """
         self.live_modules.clear()
         self.dead_modules.clear()
 
         for mod in self.modules:
             try:
-                test = requests.get('http://' + mod.lower() + '.' + self.modules_config[mod]['SERVER_NAME'])
-                if test.status_code == 200:
+                test = requests.get('http://' + mod.lower() + '.' + self.configs[mod]['SERVER_NAME'])
+                if test.status_code == 200 or 302:
                     self.live_modules.append(mod)
                 test.close()
             except Exception:
                 self.dead_modules.append(mod)
 
-        # print(f"live modules: {[m for m in self.live_modules]}")
-        # print(f"dead modules: {[m for m in self.dead_modules]}")
-
     def aggregate(self, mod):
 
         try:
-            resp = requests.get('http://' + mod.lower() + '.' + self.modules_config[mod]['SERVER_NAME'])
+            resp = requests.get('http://' + mod.lower() + '.' + self.configs[mod]['SERVER_NAME'])
 
-            if resp.status_code == 200:
+            if resp.status_code == 200 or 302:
                 self.aggregated[mod] = json.loads(resp.text)
             else:
-                del self.aggregated[mod]  # ??? why not?
+                try:
+                    del self.aggregated[mod]  # can't remove?! this will not update if module goes offline!
+                except Exception as e:
+                    map_logger.warning(f'Aggregator Warning! Module resolution failed {mod}: {e}')
+
             resp.close()
-            time.sleep(.5)
+            time.sleep(self.config.get('AGGREGATOR_TIMEOUT', .5))
 
         except Exception as e:
             map_logger.warning(f'Aggregator Warning! {e}')
@@ -83,7 +92,6 @@ class MAPAggregator(threading.Thread):
 
     def run(self):
 
-        self.configure('map.json')
         self.register_modules()
 
         while self.live_modules:
@@ -92,12 +100,12 @@ class MAPAggregator(threading.Thread):
                 self.aggregate(mod)
 
             self.register_modules()
-            print(f'aggregated: {list(self.aggregated.keys())}')
+            print(f'aggregated: {list(self.aggregated.keys())}) live: {[m for m in self.live_modules]} dead: {[m for m in self.dead_modules]}')
 
-        print("no running modules!!!")
+        map_logger.warning("no running modules!!!")
 
 
 if __name__ == '__main__':
-
     mapAgg = MAPAggregator()
+    mapAgg.configure('map.json')
     mapAgg.run()
