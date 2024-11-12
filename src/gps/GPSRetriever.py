8b1b3e0d733b973e922d6a0660a2cfe4ccd06eea
seanseancooper <sean.darwin.cooper@gmail.com>
2024-11-12 14:52:26 -0700

    made result a defaultdict that takes a dict.
    refactored TEST_FILE and this caused how it was being evaluated to slightly change. real output included in BlackViewGPSClient call (will be removed)
    
    removed gone_offline & tx_result intermediate methods. no longer needed..

--- src/gps/GPSRetriever.py
+++ src/gps/GPSRetriever.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 from datetime import datetime
 import threading
 import logging
@@ -36,7 +37,7 @@ class GPSRetriever(threading.Thread):
         self.retriever_method = None
         self.retrieving = False
         self.thread = None
-        self.result = dict()
+        self.result = defaultdict(dict)
 
     def __str__(self):
         return {f"GPSRetriever: time: {self.gps_time()} position: {self.gps_position()} config: {self.config}"}
@@ -56,31 +57,6 @@ class GPSRetriever(threading.Thread):
         except AttributeError as e:
             gps_logger.fatal(f'no retriever found {e}')
 
-    def gone_offline(self):
-        ''' emit *something* when offline '''
-        self.result['lat'] = self.lat = "0.0"
-        self.result['lon'] = self.lon = "0.0"
-        self.result['time'] = self.time = datetime.now().__format__(self.config['DATETIME_FORMAT'])
-        self.result['track'] = self.track = "0.0"
-        self.result['speed'] = self.speed = "0.0"
-        self.result['altitude'] = self.altitude = "0.0"
-        self.result['climb'] = self.climb = "0.0"
-
-        time.sleep(1)
-
-    def tx_result(self):
-
-        self.time = self.result.get('time')
-
-        self.lat = self.result.get('lat')
-        self.lon = self.result.get('lon')
-
-        self.heading = self.result.get('track')
-        self.speed = self.result.get('speed')
-        self.altitude = self.result.get('altitude')
-        self.climb = self.result.get('climb')
-
-        print(self.result)
 
     @register_retriever
     def GPSDClientRetriever(self, *, c, **retriever_args):
@@ -95,44 +71,64 @@ class GPSRetriever(threading.Thread):
                         self.result = result
 
             except OSError as e:  # unable to connect.
-                gps_logger.error(f"GPSDClientRetriever {e}")  # unable to connect to Blackview
-                self.gone_offline()
-            finally:
-                self.tx_result()
+                gps_logger.error(f"GPSDClientRetriever {e}")  # unable to connect to GPS Hardware
 
     @register_retriever
     def BlackviewGPSRetriever(self, *, c, **retriever_args):
-        # "GPS_HOST": "10.99.77.1" "GPS_PORT": 80 "GPS_CONNECT_TIMEOUT": 10
         while True:
             try:
                 with BlackViewGPSClient(**retriever_args) as client:
                     for result in client.dict_stream():
                         if not result:
                             break
-                        # {'GPS': {'LATITUDE': 39.915162, 'LONGITUDE': -105.069197, 'UPDATED': '2024-10-31 21:00:55'}}
-                        self.result = result
-                        # print(f'{self.result}')
-                        self.tx_result()
+                        # send: b'GET http://10.99.77.1/blackvue_livedata.cgi HTTP/1.1\r\n
+                        # Host: 10.99.77.1\r\n
+                        # Accept-Encoding: identity\r\n\r\n'
+                        # reply: 'HTTP/1.1 200 OK\r\n'
+                        # header: Date: Tue, 12 Nov 2024 20:33:24 GMT
+                        # header: Server: DR750S-2CH
+                        # header: Accept-Ranges: bytes
+                        # header: Connection: close
+                        # header: Content-Type: application/json; boundary=--ptaboundary
+                        # {'GPS': {'LATITUDE': 39.917143, 'LONGITUDE': -105.068342, 'UPDATED': '2024-11-12 13:33:22'}}
+                        # {'lat': 39.917143, 'lon': -105.068342, 'time': '2024-11-12 13:33:22'}
+                        # {'lat': 39.917143, 'lon': -105.068342, 'time': '2024-11-12 13:33:22'}
+                        # {'GPS': {'LATITUDE': 39.917144, 'LONGITUDE': -105.068342, 'UPDATED': '2024-11-12 13:33:22'}}
+                        # {'lat': 39.917144, 'lon': -105.068342, 'time': '2024-11-12 13:33:22'}
+                        # {'lat': 39.917144, 'lon': -105.068342, 'time': '2024-11-12 13:33:22'}
+                        # {'GPS': {'LATITUDE': 39.917145, 'LONGITUDE': -105.068343, 'UPDATED': '2024-11-12 13:33:22'}}
+                        # {'lat': 39.917145, 'lon': -105.068343, 'time': '2024-11-12 13:33:22'}
+                        # {'lat': 39.917145, 'lon': -105.068343, 'time': '2024-11-12 13:33:22'}
+                        # {'GPS': {'LATITUDE': 39.917145, 'LONGITUDE': -105.068343, 'UPDATED': '2024-11-12 13:33:22'}}
+                        # {'lat': 39.917145, 'lon': -105.068343, 'time': '2024-11-12 13:33:22'}
+                        # {'lat': 39.917145, 'lon': -105.068343, 'time': '2024-11-12 13:33:22'}
+                        # {'GPS': {'LATITUDE': 39.917146, 'LONGITUDE': -105.068344, 'UPDATED': '2024-11-12 13:33:25'}}
+                        # {'lat': 39.917146, 'lon': -105.068344, 'time': '2024-11-12 13:33:25'}
+                        # {'lat': 39.917146, 'lon': -105.068344, 'time': '2024-11-12 13:33:25'}
+                        gps_logger.debug(f'{result}')
+                        lat = result['GPS']['LATITUDE']
+                        lon = result['GPS']['LONGITUDE']
+                        updt = result['GPS']['UPDATED']
+                        self.result = {"lat": lat, "lon": lon, "time": updt}
+                        gps_logger.debug(f'{self.result}')
+                        time.sleep(self.config.get('BLACKVIEWGPSRETRIEVER_TIMEOUT', 1))
             except OSError as e:
                 gps_logger.error(f"BlackviewGPSRetriever: {e}")  # unable to connect to Blackview
-                self.gone_offline()
-            # finally:
-            #     client.close()
-            #     self.tx_result()
 
     @register_retriever
     def DummyGPSRetriever(self, *, c, **retriever_args):
 
         import json
         lines = [line.replace('LATITUDE', 'lat').replace('LONGITUDE', 'lon').strip() for line in open(self.config['TEST_FILE'], 'r')]
-        lines = [json.loads(line) for line in lines]
+        lines = [json.loads(line.replace("\'","\"")) for line in lines]
 
         while True:
             for i in range(len(lines) - 1):
-                self.result = lines[i]
-                self.result['time'] = datetime.now().__format__("%Y-%m-%d %H:%M:%S")
-                self.tx_result()
-                time.sleep(1)
+                result = lines[i]
+                lat = result['GPS']['lat']
+                lon = result['GPS']['lon']
+                self.result = {"lat": lat, "lon": lon, "time": datetime.now().__format__("%Y-%m-%d %H:%M:%S")}
+                time.sleep(self.config.get('DUMMYRETRIEVER_TIMEOUT', 1))
 
     @register_retriever
     def JavaScriptGPSRetriever(self, *, c, **retriever_args):
@@ -146,30 +142,27 @@ class GPSRetriever(threading.Thread):
 
             except Exception as e:
                 gps_logger.error(f"JavaScriptGPSRetriever: {e}")  # unable to connect to Blackview
-                self.gone_offline()
-            finally:
-                self.tx_result()
 
     def gps_time(self):
-        return str(self.time) # needs a format
+        return str(self.time)  # needs a format
 
     def gps_position(self):
         return self.result
 
     def gps_location(self):
-        return str(self.lat, self.lon)
+        return {"lat": self.result['lat'], "lon": self.result['lon']}
 
     def gps_altitude(self):
-        return str(self.altitude)
+        return {"altitude": self.result['altitude']}
 
     def gps_speed(self):
-        return str(self.speed)
+        return {"speed": self.result['speed']}
 
     def gps_track(self):
-        return str(self.heading)  # TPV class refers to 'track' as 'heading'
+        return {"heading": self.result['heading']}
 
     def gps_climb(self):
-        return str(self.climb)
+        return {"climb": self.result['climb']}
 
     def stop(self):
         pass
