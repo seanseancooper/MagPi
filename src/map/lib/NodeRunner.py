7c5cead1b05051ce453456314366e890d198852f
seanseancooper <sean.darwin.cooper@gmail.com>
2024-11-15 21:11:02 -0700

    Noderunner runs builds now. removed stop() method (it's threaded).

--- src/map/lib/NodeRunner.py
+++ src/map/lib/NodeRunner.py
@@ -1,8 +1,11 @@
 import os
+import shutil
 import subprocess
 import threading
 
 import logging
+import time
+
 from src.config import CONFIG_PATH, readConfig
 
 map_logger = logging.getLogger('gps_logger')
@@ -24,34 +27,7 @@ class NodeRunner(threading.Thread):
 
     def runOSCommand(self, command: list):
         try:
-            map_logger.info(f"[{__name__}]: running {command[3].upper()} \"{command[2]}\"")
-        except Exception as e:
-            pass  # missing element, etc. don't care
-
-        try:
-            # DBUG: This isn't safe. I need to be checking the PATH.
-            #  Warning: For maximum reliability, use a fully qualified path for the executable.
-            #
-            #  To search for an unqualified name on PATH, use shutil.which().
-            #
-            #  On all platforms, passing sys.executable is the recommended way to launch the current Python
-            #  interpreter again, and use the -m command-line format to launch an installed module.
-            #
-            #  Resolving the path of executable (or the first item of args) is platform dependent.
-            #  For POSIX, see os.execvpe(), and note that when resolving or searching for the executable
-            #  path, cwd overrides the current working directory and env can override the PATH environment
-            #  variable.
-            #
-            #  For Windows, see the documentation of the lpApplicationName and lp
-            #  CommandLine parameters of WinAPI CreateProcess, and note that when resolving
-            #  or searching for the executable path with shell=False, cwd does not override
-            #  the current working directory and env cannot override the PATH environment
-            #  variable. Using a full path avoids all of these variations.
-
-            # DBUG: the issue is that the fake systemctl isn't on the PATH and is being mis-executed
-            # print(os.environ['PATH'])
-            # os.execvpe(command[0], command[1:], os.environ)
-
+            command[0] = shutil.which(command[0])
             ps = subprocess.Popen(command)
             map_logger.debug(f"[{__name__}]: PID --> {ps.pid}")
             return ps.pid
@@ -59,19 +35,9 @@ class NodeRunner(threading.Thread):
             map_logger.error(f"[{__name__}]:couldn't create a process for \'{command}\': {e}")
         return 0
 
-    def node_stop(self):
-        command = self.config['NODE_STOP_COMMAND'].append(str(self.pid))
-
-        if self.runOSCommand(command) > 0:
-            map_logger.info(f"[{__name__}]: OK")
-        else:
-            map_logger.error(f"[{__name__}]: FAIL! Verify NODE_STOP_COMMAND: {self.config['NODE_STOP_COMMAND']} "
-                             f" is correctly formed.")
-
-    def node_build(self):
-        command = self.config['NODE_BUILD_COMMAND'].append(str(self.pid))
-
-        if self.runOSCommand(command) > 0:
+    def build(self):
+        if self.runOSCommand(self.config['NODE_BUILD_COMMAND']) > 0:
+            time.sleep(5)
             map_logger.info(f"[{__name__}]: OK")
         else:
             map_logger.error(f"[{__name__}]: FAIL! Verify NODE_BUILD_COMMAND: {self.config['NODE_BUILD_COMMAND']} "
@@ -83,6 +49,8 @@ class NodeRunner(threading.Thread):
         if self.pid > 0:
             os.chdir(home)
             map_logger.info(f"[{__name__}]: OK {self.pid}")
+
+            return self.pid
         else:
             map_logger.error(f"[{__name__}]: FAIL! Verify NODE_START_COMMAND: {self.config['NODE_START_COMMAND']} "
                              f"is correctly formed.")
