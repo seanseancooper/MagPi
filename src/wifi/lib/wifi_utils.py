19e0a8bc07575e1c23af73c24059f007ba54a2e2
seanseancooper <sean.darwin.cooper@gmail.com>
2024-11-21 20:29:35 -0700

    mooved print_signals into utils give less emphasis.

--- src/wifi/lib/wifi_utils.py
+++ src/wifi/lib/wifi_utils.py
@@ -6,6 +6,7 @@ import json
 from .iw_parse import matching_line
 from src.config import CONFIG_PATH, readConfig
 from src.lib.utils import make_path, write_file
+from src.wifi.lib.iw_parse import print_table
 
 import logging
 
@@ -56,6 +57,29 @@ def get_timing(cell):
     return time
 
 
+def print_signals(sgnls, columns):
+    table = [columns]
+
+    def print_signal(sgnl):
+        sgnl_properties = []
+
+        def make_cols(column):
+            try:
+                # make boolean a str to print (needs 'width').
+                if isinstance(sgnl[column], bool):
+                    sgnl_properties.append(str(sgnl[column]))
+                else:
+                    sgnl_properties.append(sgnl[column])
+            except KeyError as e:
+                print(f"KeyError getting column for {e}")
+
+        [make_cols(column) for column in columns]
+        table.append(sgnl_properties)
+
+    [print_signal(sgnl) for sgnl in sgnls]
+    print_table(table)
+
+
 def append_to_outfile(config, cell):
     """Append a found cells to a rolling JSON list"""
     json_logger.info({cell['BSSID']: cell})
@@ -66,3 +90,11 @@ def write_to_scanlist(config, searchmap):
     make_path(config.get('OUTFILE_PATH', "out"))
     _time = datetime.now().strftime(config.get('DATETIME_FORMAT', "%Y%m%d_%H%M%S"))
     return write_file(config['OUTFILE_PATH'], "scanlist_" + _time + ".json", json.dumps(searchmap, indent=1), "x")
+
+
+def commit_mapping(config, mapping):
+    """ commit data to a git repo, use GIT API """
+    # get_credentials(config.get('OUTFILE_PATH', "out"))
+    # _time = datetime.now().strftime(config.get('DATETIME_FORMAT', "%Y%m%d_%H%M%S"))
+    # return commit_file(config['OUTFILE_PATH'], "scanlist_" + _time + ".json", json.dumps(mapping, indent=1), "x")
+    pass
