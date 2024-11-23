8f8d7af1606e42fd3cbd345fd05c6f38a9f3f764
seanseancooper <sean.darwin.cooper@gmail.com>
2024-11-22 23:32:16 -0700

    changed naming convention of main URLs; now FORE & AFT.
    added configurable mapping for multibutton. the true, false values corrspond to the values for [_has_symbols, _has_analysis]
    
    renamed fields, simplified methods, removed comments and some logging.

--- src/cam/CAMManager.py
+++ src/cam/CAMManager.py
@@ -2,7 +2,6 @@ import threading
 import logging
 from flask import json
 
-
 from src.cam.Showxating.ShowxatingBlackviewPlugin import ShowxatingBlackviewPlugin
 from src.config.__init__ import readConfig
 
@@ -26,48 +25,45 @@ class CAMManager(threading.Thread):
 
         self.plugin = None
 
+        self.multibutton = {}
 
     def __str__(self):
         return {"CAMManager": str(self.config)}
 
     def configure(self, config_file):
-
-        if not config_file:
-            exit(1)
-
         readConfig(config_file, self.config)
 
+        # configure the multibutton
+        self.multibutton = {
+            "OFF": self.config['MULTIBUTTON_OFF'],
+            "SYM": self.config['MULTIBUTTON_SYM'],
+            "ANA": self.config['MULTIBUTTON_ANA'],
+            "ALL": self.config['MULTIBUTTON_ALL']
+        }
 
     def init_camera(self, direction):
 
-        URL = self.cam_direction(direction)
-
-        import atexit
-
-        def plugin_stops():
-            cam_logger.info(f"{self.plugin_name} plugin stopped")
-
-        atexit.register(plugin_stops)
+        # import atexit
+        #
+        # def plugin_stops():
+        #     cam_logger.info(f"{self.plugin_name} plugin stopped")
+        #
+        # atexit.register(plugin_stops)
         self.plugin = ShowxatingBlackviewPlugin()
         self.plugin.plugin_name = self.config['PLUGIN_NAME']
 
-        self.plugin.plugin_args_capture_src = URL
-        #DBUG: this shouldn't be hardcoded
-        # For webcams and many other connected cameras,
-        # you have to calculate the frames per second manually.
-        # You can read a certain number of frames from the video and see how much
-        # time has elapsed to calculate frames per second. I do this in the capture!
+        self.plugin.plugin_args_capture_src = self.cam_direction(direction)
+        # TODO: For webcams and many other connected cameras,
+        #  you have to calculate the frames per second manually.
+        #  You can read a certain number of frames from the video and see how much
+        #  time has elapsed to calculate frames per second. I do this in the capture!
 
         self.plugin.plugin_args_capture_frame_rate = 10
         self.plugin.plugin_args_capture_width = 702
         self.plugin.plugin_args_capture_height = 480
 
     def cam_direction(self, direction):
-        #TODO: return self.config[direction]; note: 'FORE'/'AFT' hardcoded in the button
-        if direction == "FORE":
-            return self.config.get('FORWARD_TEST_URL', self.config['FORWARD_VIDEO_URL'])
-        else:
-            return self.config['REVERSE_VIDEO_URL']
+        return self.config.get('FORWARD_TEST_URL', [direction.upper() + '_VIDEO_URL'])
 
     def cam_reload(self, direction):
 
@@ -89,33 +85,23 @@ class CAMManager(threading.Thread):
         self.plugin.streamservice.reload(URL)
 
     def cam_multibutton(self, mode):
-        #TODO: multibutton enumeration, less brittle.
         """ set mode of ShowxatingBlackviewPlugin """
-        if mode == 'OFF':
-            self.plugin.has_symbology = False
-            self.plugin.has_analysis = False
-        if mode == 'SYM':
-            self.plugin.has_symbology = True
-            self.plugin.has_analysis = False
-        if mode == 'ANA':
-            self.plugin.has_symbology = False
-            self.plugin.has_analysis = True
-        if mode == 'ALL':
-            self.plugin.has_symbology = True
-            self.plugin.has_analysis = True
+        try:
+            self.plugin._has_symbols, self.plugin._has_analysis = self.multibutton[mode]
+        except KeyError:
+            pass
 
-
-    def cam_plugin(self, field, value):
+    def cam_twiddle(self, field, value):
         # TODO: FIX BRITTLE CODE!
         if field == 'crop':
-
             json_value = json.loads(value)
-            self.plugin.max_height = slice(int(json_value['y']), int(json_value['h']), None)
-            self.plugin.max_width = slice(int(json_value['x']), int(json_value['w']), None)
+            self.plugin._max_height = slice(int(json_value['y']), int(json_value['h']), None)
+            self.plugin._max_width = slice(int(json_value['x']), int(json_value['w']), None)
 
         if field == 'krnl':
             self.plugin.krnl = int(value)
             self.plugin.show_krnl_grid = True
+
         if field == 'threshold':
             self.plugin.threshold = float(value)
             self.plugin.show_threshold = True
@@ -125,6 +111,7 @@ class CAMManager(threading.Thread):
                 self.plugin.sets_threshold_hold(True)
             else:
                 self.plugin.sets_threshold_hold(False)
+
         if field == 'mediapipe':
             if value == "True":
                 self.plugin.sets_mediapipe(True)
@@ -133,12 +120,14 @@ class CAMManager(threading.Thread):
 
         if field == 'f_limit':
             self.plugin.tracker.f_limit = int(value)
+
         if field == 'frame_delta':
             self.plugin.tracker.frame_delta = float(value)
+
         if field == 'frm_delta_pcnt':
             self.plugin.tracker.frm_delta_pcnt = float(value)
 
-        return "OK"
+        return True
 
     def ircam_move(self, command):
         # NOFIX:  deprecated, also brittle! [removing. 'moving a camera'
@@ -156,15 +145,14 @@ class CAMManager(threading.Thread):
             except Exception as e:
                 cam_logger.error(f"[{__name__}]:Error sending request: {str(e)}")
 
-        URL = f"{self.config['CAM']['IRCAM_HOST']}/cgi-bin/ptzBase.cgi" \
+        resp = makeRequest(f"{self.config['CAM']['IRCAM_HOST']}/cgi-bin/ptzBase.cgi" \
               f"?action=moveDirectly" \
               f"&channel=0" \
               f"&startPoint[0]={command['S_0']}" \
               f"&startPoint[1]={command['S_1']}" \
               f"&endPoint[0]={command['E_0']}" \
               f"&endPoint[1]={command['E_1']}"
-
-        resp = makeRequest(URL)
+        )
 
         if resp:
             return resp
@@ -172,7 +160,7 @@ class CAMManager(threading.Thread):
             return "IRCAM ircam_move error []"
 
     def stop(self):
-            pass
+        pass
 
     def run(self):
         self.init_camera("FORE")
