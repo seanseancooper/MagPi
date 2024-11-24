64e85ab45af66467bb75e25c61408ab6a448d8f9
seanseancooper <sean.darwin.cooper@gmail.com>
2024-11-23 23:50:16 -0700

    minor changes to logic; I suspect this works badly, and will reevaluate it after moving all the junk.

--- src/cam/Showxating/lib/StreamService.py
+++ src/cam/Showxating/lib/StreamService.py
@@ -11,9 +11,9 @@ cam_logger = logging.getLogger('cam_logger')
 
 
 def highlight(arr, x, y):
+    """sample a pixel position; use combinatorial 'inversion'
+    of the color to produce a new color that is always visible."""
     from PIL import Image
-    # sample a pixel position and use a combinatorial 'inversion'
-    # of the color value, so it's always visible.
     img = Image.fromarray(arr, "RGB")
     pix = img.load()
     R, G, B = pix[x, y]
@@ -24,55 +24,51 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
 
     def __init__(self, request: bytes, client_address: Tuple[str, int], srvr: socketserver.BaseServer):
         super().__init__(request, client_address, srvr)
-
-        # Class attributes
         self.src = None
-        self.config_path = None
-
-        cam_logger.debug(f"{threading.current_thread().name} created StreamingHandler for request.")
+        self.context = None
+        self.highlight = ()
 
     def do_GET(self):
 
-        if self.path == self.config_path:
+        if self.path == self.context:
+
             self.send_response(200)
             self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=--jpgboundary')
             self.send_header('Cache-Control',
                              'no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0')
             self.end_headers()
 
-            def process_frame(self, f):
+            if type(self.src) is str:  # a new URL
+                return
 
-                if type(f) is str:
-                    # cam_logger.debug(f"{threading.current_thread().name} StreamService received {self.src} URL in process")
-                    return  # we got a new URL, ignore
+            if type(self.src) is np.ndarray:  # a plugin frame
 
-                try:
-                    # VideoCapture yields ndarray NOT a jpeg; make a jpeg
-                    params = (cv.IMWRITE_JPEG_QUALITY, 100)
-
-                    img_str = cv.imencode('.jpeg', f, params)[1].tobytes()
-
-                    self.wfile.write(b"\r\n--jpgboundary\r\n")
-                    self.send_header('Content-type', 'image/jpeg')
-                    self.send_header('Content-length', str(len(img_str)))
-                    self.send_header('Access-Control-Expose-Headers', 'majic-color')
-                    self.send_header('majic-color', str(highlight(f, 10, 110)))
-                    self.end_headers()
-
-                    self.wfile.write(img_str)
-                except ConnectionResetError as e:
-                    exit(0)
-                except BrokenPipeError as e:
-                    exit(0)
-
-            if type(self.src) is str:
-                # cam_logger.debug(f"{threading.current_thread().name} StreamService received {self.src} URL")
-                return  # we got a new URL, ignore
-
-            if type(self.src) is np.ndarray:
-                # output from a plugin (an ndarray, aka 'frame').
-                # let while 'do it's thing' as long as we have something...
                 while self.src is not None:
+
+                    self.highlight = highlight(self.src, 10, 110)
+
+                    def process_frame(sh, f):
+
+                        if type(f) is str:
+                            return  # a new URL
+
+                        try:
+                            p = (cv.IMWRITE_JPEG_QUALITY, 100)
+                            img_str = cv.imencode('.jpeg', f, p)[1].tobytes()
+
+                            sh.wfile.write(b"\r\n--jpgboundary\r\n")
+                            sh.send_header('Content-type', 'image/jpeg')
+                            sh.send_header('Content-length', str(len(img_str)))
+                            sh.send_header('Access-Control-Expose-Headers', 'majic-color')
+                            sh.send_header('majic-color', str(self.highlight))
+                            sh.end_headers()
+
+                            sh.wfile.write(img_str)
+                        except ConnectionResetError:
+                            pass
+                        except BrokenPipeError:
+                            pass
+
                     process_frame(self, self.src)
 
         else:
@@ -80,58 +76,54 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.end_headers()
 
 
-# This class is identical to HTTPServer but uses threads to handle requests by using the ThreadingMixIn. This
-# is useful to handle web browsers pre-opening sockets, on which HTTPServer would wait indefinitely.
 class StreamService(server.ThreadingHTTPServer):
 
-    def __init__(self, address, config_path, requesthandler):
-        super(StreamService, self).__init__(address, requesthandler)
+    def __init__(self, address, context, handler):
+        super(StreamService, self).__init__(address, handler)
 
         self.address = address
-        self.requesthandler = requesthandler
+        self.context = context
+        self.handler = handler
 
-        self.allow_reuse_address = True     # Whether the server will allow the reuse of an address. This defaults to False, and can be set in subclasses to change the policy.
-        self.daemon_threads = True          # Use daemonic threads by setting ThreadingMixIn.daemon_threads to True to not wait until threads complete.
+        self.allow_reuse_address = True     # will server will allow the reuse of an address
+        self.daemon_threads = True          # True not wait until threads complete.
 
         self.src = None
-        self.service = None
-
-        self.config_path = config_path
 
     def reload(self, new_src):
         """ change direction """
         self.shutdown()
 
-        handler = StreamingHandler  # make a NEW instance of handler.
-        handler.src = new_src  # set the handlers' 'source' to the new_src.
-        self.requesthandler = handler
+        h = StreamingHandler      # make a NEW instance of handler.
+        h.src = new_src           # set the handlers' 'source' to the new_src.
+        self.handler = h
 
-        # BAU
         self.src = new_src
-        cam_logger.debug(f"{threading.current_thread().name} StreamService set new src {new_src} and reloaded!")
         self.stream()
 
     def stream(self):
 
-        self.requesthandler.config_path = self.config_path
+        self.handler.context = self.context
 
         try:
             threading.Thread(target=self.serve_forever, name="streamservice").start()
-            cam_logger.info(f"{threading.current_thread().name} streaming on http://{self.address[0]}:{self.address[1]}{self.config_path}")
+            cam_logger.info(f"{threading.current_thread().name} streaming on http://{self.address[0]}:{self.address[1]}{self.context}")
         except Exception as e:
-            cam_logger.error(f"streaming error on http://{self.address[0]}:{self.address[1]}{self.config_path}! " + str(e))
+            cam_logger.error(f"streaming error on http://{self.address[0]}:{self.address[1]}{self.context}! " + str(e))
 
 
 if __name__ == "__main__":
     import os
     from src.config.__init__ import readConfig, CONFIG_PATH
 
-    myconfig = {}
-    readConfig(os.path.join(CONFIG_PATH, 'cam.json'), myconfig)
-
-    handler = StreamingHandler  # choose the handler.
-    handler.src = myconfig.get('FORWARD_TEST_URL', myconfig['FORWARD_VIDEO_URL'])  # set the handlers' 'source'.
-    svc = StreamService(('cam.localhost', 5002), '/stream', handler)
-    svc.stream()
+    # myconfig = {}
+    # readConfig(os.path.join(CONFIG_PATH, 'cam.json'), myconfig)
+    #
+    # handler = StreamingHandler  # choose the handler.
+    # handler.src = myconfig.get('FORWARD_TEST_URL', myconfig['FORE_VIDEO_URL'])  # set the handlers' 'source'.
+    # host, port = myconfig.get('SERVER_NAME').split(':')
+    # svc = StreamService((host, int(port)), '/stream', handler)
+    # svc.src = handler.src
+    # svc.stream()
 
 
