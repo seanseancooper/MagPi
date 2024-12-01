b166876b1a2b9ae6187af232c0e2d545a5201c84
seanseancooper <sean.darwin.cooper@gmail.com>
2024-12-01 12:07:45 -0700

    added some notes based on design cycle; the issue is having the right data/type in the right place at the right time. the FrameObjekt is the root of everything; I use it to encapsulate the notion of what 'just happened' and it is used in most ccalculations.  Inefficiencies in the design ccan cause a lot of logistical 'arm twinsting' and this results in errors and unnecessary processing.
    The goal is to find these and root them out; the first step is getting the right types.
    
    here we go.

--- src/cam/Showxating/lib/FrameObjekt.py
+++ src/cam/Showxating/lib/FrameObjekt.py
@@ -6,23 +6,23 @@ class FrameObjekt:
     def __init__(self, f_id):
 
         # metadata
-        self.frame_id = f_id
-        self.timestamp = datetime.now()  # not used yet
-        self.tag = None
-        self.isNew = True
-        self.skip = False
+        self.f_id = f_id                                # frame id when created.
+        self.timestamp = datetime.now()                 # not used yet
+        self.tag = None                                 # [deprecate this in favor of using the f_id, portable]   string unique identifier for this frame
+        self.isNew = True                               # boolean is the id a NEW id or derived from f[-1]
+        self.skip = False                               # [is this logical to have, useful or relevant?] should this frame be skipped in tagging f[+1]
 
-        # frame data: features
-        self.contours = None   # ALL contours in this frame
-        self.hierarchy = None  # ordering of the contours in this frame
-        self.prev_tag = None   # tag of nearest FrameObjekt from the previous frame
-        self.prev_dist = 0.0   # euclidean_distance wrt previous mean x, y location
-        self.distances = np.array([], dtype=np.float64)    # list of previous euclidean_distance wrt previous mean x, y locations.
-        self.fd = 0.0          # euclidean_distance wrt previous frame analysis area
-        self.rect = None       # bounding rects of contours in this frame
-        self.ml = None         # mean x, y location of *this* contour
-        self.wall = None       # image of processed area in this frame
-        self.close = False
+        # frame data: features [remove unused things, deccide when things are used]
+        self.contours = None                            # [list: object tracking] ALL contours in this frame
+        self.hierarchy = None                           # [list: unused] ordering of the contours in this frame
+        self.prev_tag = None                            # [string: object tracking] tag of nearest FrameObjekt from the previous frame
+        self.prev_dist = 0.0                            # [float: object tracking] euclidean_distance wrt previous mean x, y location
+        self.distances = np.array([], dtype=np.float64) # [list: object location] of previous euclidean_distances wrt previous mean x, y locations.
+        self.fd = 0.0                                   # [float: reporting] euclidean_distance wrt previous frame analysis area
+        self.rect = None                                # [list: object segmentation] bounding rects of contours in this frame
+        self.ml = None                                  # [list: object segmentation] mean x, y location of *this* contour
+        self.wall = None                                # [ndarray: container] image of processed area in this frame
+        self.close = False                              # [boolean: reporting] is this mean location with the bounds of the contour?
 
     @staticmethod
     def create(f_id):
@@ -36,7 +36,7 @@ class FrameObjekt:
         self.wall = wall
 
     def __str__(self):
-        return {'f_id' : self.frame_id,
+        return {'f_id' : self.f_id,
                 'timestamp': self.timestamp,
                 'tag'      : self.tag,
                 'isNew'    : self.isNew,
