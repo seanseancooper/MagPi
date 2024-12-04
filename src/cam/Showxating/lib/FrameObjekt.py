a666d7961bfbb05d910891bdd2368d718328db7f
seanseancooper <sean.darwin.cooper@gmail.com>
2024-12-04 10:41:38 -0700

    defined some types; I'm moving aroound what is available when so I ccan better manage FrameObjekt creation & use.

--- src/cam/Showxating/lib/FrameObjekt.py
+++ src/cam/Showxating/lib/FrameObjekt.py
@@ -8,23 +8,24 @@ class FrameObjekt:
     def __init__(self, f_id):
 
         # metadata
-        self.f_id = f_id                                # frame id when created.
-        self.timestamp = datetime.now()                 # not used yet
-        self.tag = None                                 # [deprecate this in favor of using the f_id, portable] string unique identifier for this frame
-        self.isNew = True                               # boolean is the id a NEW id or derived from f[-1]
-        self.skip = False                               # [is this logical to have, useful or relevant?] should this frame be skipped in tagging f[+1]
+        self.f_id = f_id                                    # frame id when created.
+        self.timestamp = datetime.now()                     # not used yet
+        self.tag = None                                     # [deprecate this in favor of using the f_id, portable] string unique identifier for this frame
+        self.isNew = True                                   # boolean is the id a NEW id or derived from f[-1]
+        self.skip = False                                   # [is this logical to have, useful or relevant?] should this frame be skipped in tagging f[+1]
 
         # frame data: features [remove unused things, decide when things are used]
-        self.contours = None                            # [tuple of ndarray(n, 1, 2): object tracking] ALL contours in this frame
-        self.hierarchy = None                           # [ndarray  1, n, 4: unused] ordering of the contours in this frame  (UNUSED)
-        self.prev_tag = ''                              # [string: object tracking] tag of nearest FrameObjekt from the previous frame
-        self.curr_dist = float()                        # [float64: object tracking] euclidean_distance wrt previous mean x, y location
-        self.distances = np.ndarray(shape=[1, 1], dtype=np.float64)  # [list([1,1]): object location] of previous euclidean_distances wrt previous mean x, y locations.
-        self.fd = float()                               # [float: reporting] euclidean_distance wrt previous frame analysis area
-        self.rect = None                                # [tuple {x, y, w, h}: object segmentation] bounding rects of contours in this frame
-        self.ml = np.ndarray(shape=[2], dtype=np.int64) # [tuple {x,y}: object segmentation] mean x, y location of *this* contour
-        self.wall = None                                # [ndarray: container] image of processed area in this frame
-        self.close = None                               # [boolean: reporting] is this mean location with the bounds of the contour?
+        self.contours = None                                            # [tuple of ndarray(n, 1, 2): object tracking] ALL contours in this frame
+        self.hierarchy = None                                           # [ndarray  1, n, 4: unused] ordering of the contours in this frame  (UNUSED)
+        self.prev_tag = ''                                              # [string: object tracking] tag of nearest FrameObjekt from the previous frame
+        self.curr_dist = 0                                              # [int32: object tracking] euclidean_distance wrt previous mean x, y location
+        self.distances = np.zeros(shape=[1, 1], dtype=np.float64)     # [list([1,1]): object location] of previous euclidean_distances wrt previous mean x, y locations.
+        self.fd = float()                                               # [float: reporting] euclidean_distance wrt previous frame analysis area
+        self.rect = None                                                # [tuple {x, y, w, h}: object segmentation] bounding rects of contours in this frame
+        self.ml = np.ndarray(shape=[2,], dtype=np.int32)                # [tuple {x,y}: object segmentation] mean x, y location of *this* contour
+        self.md = float()                                               # mean of self.distances
+        self.wall = None                                                # [ndarray: container] image of processed area in this frame
+        self.close = None                                               # [boolean: reporting] is this mean location with the bounds of the contour?
         self.is_inside = None
 
     @staticmethod
