d50bfcb075f7ad021cbc48e03ade8ccc443300c9
seanseancooper <sean.darwin.cooper@gmail.com>
2024-12-10 23:24:50 -0700

    this works, but there are items in the map that currently need formatting and ccan't be jsonified.
    Will trak those down, but POC works.

--- src/cam/routes.py
+++ src/cam/routes.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, jsonify
 
 from src.cam.CAMManager import CAMManager
 
@@ -18,21 +18,29 @@ cam_bp = Blueprint(
 @cam_bp.route('/')
 def index():
     # TODO: what 'data' can be exposed instead of config?
+    plugin = camMgr.plugin.get()
     #  capture stats
     #  plugin config
-    #  tracking hyperparameter config
+
+    tracker = camMgr.plugin.tracker.get()
     #  tag
     #  tag location
     #  self._ml = []                        # DO NOT CHANGE: list of (x,y) location of contour in self.contours
     #  self._frame_delta                    # DO NOT CHANGE: euclidean distance between the current and previous frames
     #  self._frame_MSE = float()
     #  self._frame_SSIM = float()
-    #  motion detected
-    #  analytics: last n frames
-    plugin = camMgr.plugin.get()
-    tracker = camMgr.plugin.tracker.get()
+    #  self.has_motion                      # motion detected
+    #  analytics: last n tracked frames see
+    #      if has_analysis:
+    #         for w, _ in enumerate([x for x in t][:1], 1):
+    #             o = t.get(_)
+    #             try:
+    #                 cv.putText(f, o.tag, (385, 345 + (w * 20)), cv.FONT_HERSHEY_PLAIN, .75, (255, 255, 255), 1)
+    #             except AttributeError as a:
+    #                 pass
+
     streamservice = camMgr.plugin.streamservice.get()
-    return plugin, tracker, streamservice
+    return jsonify(streamservice)
 
 
 @cam_bp.route("/ctrl", methods=['GET'], subdomain='cam')
