fda2fd4c6bdebc8b23fb799dbb41dcfc3be19546
seanseancooper <sean.darwin.cooper@gmail.com>
2024-12-13 01:00:34 -0700

    EBS lost its' controller; we don't need it, or actual routes, templates, static files or any of the REST stuff. What was in __init__.py took a hike too.
    This doesn't need this stuff; as it turns out I was mistaken about what app context can provide if there is not overarching context (an idea who's time has passed) to glue it together. VIEW may be come that, but probably not as I have other plans.
    
    The change to routes leaves only a configured EBSManager for another class to use.

--- src/ebs/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.ebs.EBSController import EBSController
-from src.lib.rest_server import RESTServer as RESTServer
-
-if __name__ == '__main__':
-    e = EBSController()
-    RESTServer(e.create_app()).run()
-    e.run()
--- src/ebs/routes.py
+++ src/ebs/routes.py
@@ -1,42 +1,6 @@
-from flask import Blueprint, render_template, request
-
+# MANDATORY FOR NOW:
+# this allows the Manager creation to
+# separate from the class that instanced it.
 from src.ebs.EBSManager import EBSManager
-
 ebsMgr = EBSManager()
 ebsMgr.configure('ebs.json')
-
-
-ebs_bp = Blueprint(
-        'ebs_bp', __name__, subdomain='ebs',
-        template_folder=ebsMgr.config['TEMPLATE_FOLDER'],
-        static_folder=ebsMgr.config['STATIC_FOLDER'],
-)
-
-
-@ebs_bp.route('/', subdomain='ebs')
-def index():
-    return ebsMgr.config
-
-
-@ebs_bp.route('/enunciate', methods=['GET', 'POST'], subdomain='ebs')
-def ebs_enunciate():
-
-    if request.method == 'POST':
-        # result = request.form
-        # json_result = dict(result)
-        msg = request.form.get("msg", "")
-
-        if msg:
-           ebsMgr.enunciate(str(msg))
-
-    return render_template("ebs.html.j2")
-
-
-@ebs_bp.route('/run', methods=['GET', 'POST'], subdomain='ebs')
-def ebs_run():
-    return ebsMgr.run()
-
-
-@ebs_bp.route("/stop", methods=['GET', 'POST'], subdomain='ebs')
-def ebs_stop():
-    return ebsMgr.stop()
