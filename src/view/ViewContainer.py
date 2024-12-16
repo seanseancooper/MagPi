2c5d770996b66062de2ea2d7ed3c7c941d6989ff
seanseancooper <sean.darwin.cooper@gmail.com>
2024-12-16 08:22:24 -0700

    added fields to support templating modules/module_tabs

--- src/view/ViewContainer.py
+++ src/view/ViewContainer.py
@@ -1,8 +1,6 @@
 import threading
 import time
 from datetime import datetime, timedelta
-
-
 from src.config import readConfig
 
 
@@ -14,14 +12,39 @@ class ViewContainer(threading.Thread):
         self.created = datetime.now()
         self.updated = datetime.now()
         self.elapsed = timedelta()
+        self.view_timeout = 0
+        self.modules = ['wifi', 'trx']
+
+        # read fragments in module templates dir.
+        # fragment name is module_tab
+        # templates/
+        #   post    <-- anything before any fragment (js or html)
+        #   pre     <-- anything after any fragment (js only)
+        #   wifi.j2    <-- the fragment (js+html)
+        #   tracked.j2
+        #   ghosts.j2
+
+        self.module_tabs = {
+                                'wifi': ['wifi', 'tracked', 'ghosts'],
+                                'trx': ['trx']
+                            }
+
         self.config = {}
 
     def stop(self):
         print(f'stopping.')
-        pass
 
     def configure(self, config_file):
         readConfig(config_file, self.config)
+        self.view_timeout = self.config.get('VIEW_TIMEOUT')
+
+    def add_module(self, m):
+        self.modules.append(m)
+        print(f'added module {m}')
+
+    def del_module(self, m):
+        self.modules.remove(m)
+        print(f'removed module {m}')
 
     @staticmethod
     def get_view_container(app):
