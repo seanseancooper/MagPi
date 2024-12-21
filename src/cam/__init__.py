5bdaba7f3e91f09860c7f7e4549864ad8c495d63
seanseancooper <sean.darwin.cooper@gmail.com>
2024-12-20 20:13:08 -0700

    removed; doesn't work anymore.

--- src/cam/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.cam.CAMController import CAMController
-from src.lib.rest_server import RESTServer as RESTServer
-
-if __name__ == '__main__':
-    c = CAMController()
-    RESTServer(c.create_app()).run()
-    c.run()
