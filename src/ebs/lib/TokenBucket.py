0eb927cbdd739958fbeaed35811037c9f8ecdec0
seanseancooper <sean.darwin.cooper@gmail.com>
2024-12-22 17:00:47 -0700

    removed DEBUG and __str__

--- src/ebs/lib/TokenBucket.py
+++ src/ebs/lib/TokenBucket.py
@@ -16,9 +16,6 @@ class TokenBucket:
     def __repr__(self):
         return f"tokens:{self.tokens} time_unit:{self.time_unit} bucket:{self.bucket} last_check:{self.last_check}"
 
-    def __str__(self):
-        return f"tokens:{self.tokens} time_unit:{self.time_unit} bucket:{self.bucket} last_check:{self.last_check}"
-
     def handle(self, message):
 
         if message:
@@ -26,8 +23,7 @@ class TokenBucket:
             time_passed = current - self.last_check
             self.last_check = current
 
-            if self.DEBUG:
-                wifi_logger.debug(f"[{__name__}]: TokenBucket contains: {self.bucket}: {time_passed}")
+            print(f"[{__name__}]: TokenBucket contains: {self.bucket}: {time_passed}")
 
             # DBUG: convert self.bucket to a timedelta and move this internal to WifiScanner
             self.bucket = self.bucket + time_passed * (self.tokens / self.time_unit)
@@ -36,11 +32,9 @@ class TokenBucket:
                 self.bucket = self.tokens
 
             if self.bucket < 1:
-                if self.DEBUG:
-                    wifi_logger.warning(f"[{__name__}]: TokenBucket message dropped: {str(message)} bucket contains: {self.bucket}")
+                print(f"[{__name__}]: TokenBucket message dropped: {str(message)} bucket contains: {self.bucket}")
                 return None
             else:
                 self.bucket = self.bucket - 1
-                if self.DEBUG:
-                    wifi_logger.warning(f"[{__name__}]: TokenBucket message returned: {str(message)} bucket contains: {self.bucket}")
+                print(f"[{__name__}]: TokenBucket message returned: {str(message)} bucket contains: {self.bucket}")
                 return message
