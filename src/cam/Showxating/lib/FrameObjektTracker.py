a43b02da261ced6cb3336256c377a408ceb90ebe
seanseancooper <sean.darwin.cooper@gmail.com>
2024-12-25 16:09:13 -0700

    this latest  version seems to trak items well.
    removed self.contour
    added method to calculate MSE.
    added fields to printed output.
    
    label_locations now takes frame, wall, rectangle and does all the work of calculation and field setting.
    
    added comapre_methood and norm_type to hist_plugin config. Hardcoded for now.
    
    track_objects had all it's guts moved to label_locations. Gotta change that name.
    
    Notably, I am observing changes in the signals that indicate corresponding changes in the data.
    
    committing this as next refactoring.

--- src/cam/Showxating/lib/FrameObjektTracker.py
+++ src/cam/Showxating/lib/FrameObjektTracker.py
@@ -2,11 +2,12 @@ import uuid
 
 import cv2 as cv
 import numpy as np
+from decimal import *
 from src.cam.Showxating.lib.FrameObjekt import FrameObjekt
 from sklearn.metrics import euclidean_distances, pairwise_distances
 from sklearn.metrics.pairwise import paired_distances
 
-from src.cam.Showxating.lib.utils import in_range, is_inside, getAggregatedRect, getRectsFromContours
+from src.cam.Showxating.lib.utils import is_in_range, is_inside, getAggregatedRect, getRectsFromContours
 from src.config import readConfig
 
 import logging
@@ -25,7 +26,6 @@ class FrameObjektTracker:
         self.f_delta_pcnt = 0.50            # hyperparameter: 0..1 percentage of delta between the current and previous frames over all pixels in frame
         self.l_delta_pcnt = 0.50            # hyperparameter: 0..1 percentage of delta between the current and previous mean locations
         self.contour_limit = None           # number of contours evaluated by plugin in each pass
-        self.contour = None                # ????
         self.contour_id = None              # ????
         self.tracked = {}                   # mapping of FrameObjekts over last 'f_limit' frames.
         self.greyscale_frame  = None
@@ -76,6 +76,7 @@ class FrameObjektTracker:
                 print(f'bad l_delta_pcnt: (should be <= 1.0)')
                 exit(1)
 
+           # not implmented. yet...
             self.contour_limit = int(self.config['TRACKER'].get('contour_limit', None))
             if self.contour_limit == 0:
                 print(f'contour_limit warning: (contour_limit should be > 0 or "None" for all contours)')
@@ -105,10 +106,16 @@ class FrameObjektTracker:
             self._frame_delta = np.mean(pairwise_distances(X, Y))
             self._frame_deltas.append(self._frame_delta)
 
-            self._frame_MSE = np.sum((X.astype("float") - Y.astype("float")) ** 2)
+            self._frame_MSE = np.sum((X - Y) ** 2)
             self._frame_MSE /= float(X.shape[0] * Y.shape[1])
 
-            from skimage.metrics import structural_similarity as ssim
+            def MSE(X, Y):
+                n = X.shape[0] * Y.shape[1]
+                return 1 / n * np.sum(np.square(X - Y))
+
+            self._frame_SSIM = MSE(X, Y)
+
+            # from skimage.metrics import structural_similarity as ssim
             # self._frame_SSIM = ssim(X, Y)
             # self._frame_MSEs.append(self._frame_MSE)
 
@@ -122,14 +129,20 @@ class FrameObjektTracker:
 
     def print_frame(self, o, origin):
         # print(f"{str(self.f_id)}\t{origin}{o.contour_id}-{str(o.tag[-12:])}\t"
-        print(f"{str(self.f_id)}\t{origin}{str(o.tag)}\t"
-        #       f"INSIDE: {str(o.is_inside).ljust(2, ' ')}\t"
-        #       f"CLOSE: {str(o.close).ljust(1, ' ')}\t"
-              f"dist: {str(o.curr_dist.__format__('.4f')).ljust(3, ' ')}\t"
-              f"md: {str(o.md.__format__('.4f')).ljust(3, ' ')}\t"
-              f"ML: {str(o.ml)}\t"
-              # f"rect:{str(o.rect).ljust(10, ' ')}\t"
-              # f"fd: {str(self.fd_mean.__format__('.4f')).ljust(10, ' ')}\tmse: {str(self._frame_MSE.__format__('.4f')).ljust(10, ' ')}\tssim: {str(self._frame_SSIM.__format__('.4f')).ljust(10, ' ')}"
+        print(f"{self.f_id}"
+              f"\t{origin}"
+              f"\t{o.tag}"
+              f"\to.curr_dist: {str(o.curr_dist.__format__('.4f')).ljust(3, ' ')}"
+              f"\to.md: {str(o.md.__format__('.4f')).ljust(3, ' ')}"
+              f"\to.ml: {str(o.ml)}"
+              f"\to.rect:{str(o.rect).ljust(10, ' ')}\t"
+              f"\to.fd: {str(self.fd_mean.__format__('.4f')).ljust(10, ' ')}"
+              f"\tmse: {str(self._frame_MSE.__format__('.4f')).ljust(10, ' ')}"
+              f"\to.in_range: {o.in_range} "
+              f"\to.is_inside: {o.is_inside} "
+              f"\to.close: {o.close} "
+              f"\to.hist_delta: {o.hist_delta} "
+              f"\to.is_negative: {o.is_negative}"
         )
 
     def get_mean_location(self, contours):
@@ -140,7 +153,7 @@ class FrameObjektTracker:
         [(x.append(a), y.append(b)) for [[a, b]] in contours]
         return np.mean(np.array([x, y]), axis=1, dtype=int)
 
-    def label_locations(self):
+    def label_locations(self, frame, wall, rectangle):
         """ find elements 'tag' by euclidean distance """
 
         labeled = []
@@ -148,83 +161,131 @@ class FrameObjektTracker:
         p_ml = [self.tracked.get(o_tag).ml for o_tag in list(self.tracked.keys())]  # get all previous mean_locations (if they exist)
 
         if len(p_ml) == 1:
-            # NEW THING
-            o = FrameObjekt.create(self.f_id)
+            # NEW TAG for a different thing; there is only one
+            o1 = FrameObjekt.create(self.f_id)
+            o1.rect = rectangle  # NOW WE GET A RECT.
+            o1.wall = wall                               # the current wall is my wall.
 
-            o.ml = self._ml
-            o.isNew = True
+            o1.ml = self._ml
+            o1.isNew = True
 
-            o.contour_id = self.contour_id
+            o1.contour_id = self.contour_id
             # contours are sorted, first p_ml is 'largest' contour
-            o.distances = euclidean_distances(np.array([self._ml], dtype=int), np.array([p_ml[0]], dtype=int).reshape(1, -1))
-            o.md = np.mean(o.distances)
+            o1.distances = euclidean_distances(np.array([self._ml], dtype=int), np.array([p_ml[0]], dtype=int).reshape(1, -1))
+            o1.md = np.mean(o1.distances)
             # popitem removes; is that really what we want?
-            o.prev_tag = str(list(self.tracked.keys())[0])                 # find the last thing with a tag, if not expired already ("when?", f_limit). LEAVE IT IN THERE!
-            o.curr_dist = int(o.distances[0])
+            o1.prev_tag = str(list(self.tracked.keys())[0])                 # find the last thing with a tag, if not expired already ("when?", f_limit). LEAVE IT IN THERE!
+            o1.curr_dist = int(o1.distances[0])
 
-            o.tag = o.create_tag(self.f_id)  # NEW TAG
+            o1.tag = o1.create_tag(self.f_id)
+            o1.is_inside = is_inside(o1.ml, o1.rect)
+            o1.close = is_in_range(o1.curr_dist, o1.md, self.l_delta_pcnt * o1.md)
 
-            self.print_frame(o, "N1:")
+            self.print_frame(o1, "N1:")
             speech_logger.info('NEW')
-            labeled.append(o)
+            labeled.append(o1)
 
         if len(p_ml) > 1:
-            o = FrameObjekt.create(self.f_id)
+            oN = FrameObjekt.create(self.f_id)
+            oN.rect = rectangle  # NOW WE GET A RECT.
+            oN.wall = wall                               # the current wall is my wall.
 
-            o.contour_id = self.contour_id
+            oN.contour_id = self.contour_id
+            oN.ml = self._ml
+            oN.isNew = False  # could be reset!
 
-            o.ml = self._ml
-            o.isNew = False  # could be reset!
             # get the mean location of the distances identified in the previous frame
-            o.distances = [euclidean_distances(np.array([self._ml], dtype=int), np.array([p_ml[j]], dtype=int).reshape(1, -1)) for j in np.arange(len(p_ml)) if p_ml[j] is not None]
-            o.md = np.mean(o.distances)
+            oN.distances = [euclidean_distances(np.array([self._ml], dtype=int), np.array([p_ml[j]], dtype=int).reshape(1, -1)) for j in np.arange(len(p_ml)) if p_ml[j] is not None]
+            oN.md = np.mean(oN.distances)
+
             # I think this is wrong; criteria should add:
-            # if it exists, it should be the item w/ a matching tag (don't have that, so closest)
             # if distance from the closest is > the bounds of the current rect, it is new.
             # this should choose the closest item within the bounds of it's rect.
-            idx = np.argmin(o.distances)                              # the item in the array representing the minimum euclidean distance to the current location
+            idx = np.argmin(oN.distances)                                # the item in the array representing the minimum euclidean distance to the current location
+            oN.curr_dist = float(oN.distances[idx])                       # distance from last location
 
-            o.prev_tag = str(list(self.tracked.keys())[idx])          # target tag; may not be the right tag
-            o.curr_dist = float(o.distances[idx])                     # distance from last location
+            oN.prev_tag = str(list(self.tracked.keys())[idx])            # target tag; may not be the right tag
+            oN.tag = f"{self.f_id}_{oN.prev_tag.split('_')[1]}"           # THIS IS A GUESS
+            # did o suddenly appear?
 
-            # DEFER TAG CREATION
-            o.tag = f"{self.f_id}_{o.prev_tag.split('_')[1]}"
+            # is the current ml inside the current rect? THIS SHOULD ALWAYS BE TRUE
+            # if so -- this is MY RECTANGLE, otherwise ths  is indicative of some other contour.
+            oN.is_inside = is_inside(oN.ml, oN.rect)
 
-            self.print_frame(o, "   ")
-            labeled.append(o)
+            # is the current distance within a range of the
+            # median of distances for the last thing with a tag?
+            # if NOT, then this is the wrong rect, use the tag of the previous
+            # is the current distance larger than the width of the thing??
+            oN.close = is_in_range(oN.curr_dist, oN.md, self.l_delta_pcnt * oN.md)
+
+            # euclidean distance of the wall histograms
+            # observation: this goes negative periodically, apparently
+            # on tags that might be in the set of things
+            # we don't want. it *often* precedes a tag that has
+            # a 'higher' percentage.
+
+            oN.hist_delta = self.get_histogram_delta(self.tracked.get(oN.prev_tag).wall, wall, rectangle)
+
+            oN.is_negative = False
+            if Decimal.from_float(oN.hist_delta).is_signed():
+                oN.is_negative = True
+
+            # pairwise distance to the previous wall image
+            X = self.make_grey_data(self.tracked.get(oN.prev_tag).wall, rectangle)
+            Y = self.make_grey_data(wall, rectangle)
+            self.set_frame_delta(X, Y)  # compare to current wall
+
+            oN.fd = self._frame_delta  # delta of wall image to current f
+            self.fd_mean = np.mean(self._frame_deltas)  # a float,
+            self.d_range = self.f_delta_pcnt * self.fd_mean  # percentage of px difference
+
+            oN.in_range = False
+            if is_in_range(oN.fd, self.fd_mean, self.d_range):
+                oN.in_range = True
+
+            # does this f match the previous f
+            # and thus the previous tag? make an
+            # evaluation based on delta between frames.
+
+            self.print_frame(oN, "   ")
+            labeled.append(oN)
 
         if not labeled:                                             # nothing in cache.
             # f 0
             o = FrameObjekt.create(self.f_id)
+            o.rect = rectangle          # the current rectangle is my rect.
+            o.wall = wall               # the current wall is my wall.
             o.ml = self._ml
+            o.is_inside = is_inside(o.ml, o.rect)
+            o.close = is_in_range(o.curr_dist, o.md, self.l_delta_pcnt * o.md)
+            o.hist_delta = None
             o.skip = True
+            o.tag = o.create_tag(self.f_id)
             self.print_frame(o, "N0:")
             labeled.append(o)
 
         return labeled
 
-    def get_histograms(self, frame, wall, rectangle, metric):
+    def get_histogram_delta(self, frame, wall, rectangle):
         from src.cam.Showxating.ShowxatingHistogramPlugin import ShowxatingHistogramPlugin
 
         hist_plugin = ShowxatingHistogramPlugin()
         hist_plugin.plugin_name = 'histogramPlugin'
         hist_plugin.get_config()
         hist_plugin.f_id = self.f_id
-        hist_plugin._kz = (3,3) # self.kernel_sz
+        hist_plugin._kz = (3, 3)  # self.kernel_sz
         hist_plugin.library = 'cv'  # TODO: add to configurable
-        hist_plugin.greyscale_frame = self.greyscale_frame
         hist_plugin.rectangle = rectangle
+        hist_plugin.compare_method = cv.HISTCMP_CORREL
+        hist_plugin.norm_type = cv.NORM_MINMAX
 
-        # gaussian kernel pre processing
-        f_hist = hist_plugin.make_histogram(frame, rectangle)
-        w_hist = hist_plugin.make_histogram(wall, rectangle)
+        f_hist = hist_plugin.get_histogram(frame, rectangle)
+        w_hist = hist_plugin.get_histogram(wall, rectangle)
 
-        if self.config['PLUGIN'].get('color_histograms', False):
-            dists = hist_plugin.compare_hist(f_hist, w_hist, metric=metric)
-        else:
-            dists = hist_plugin.compare_hist(f_hist['greyscale'], w_hist['greyscale'], metric=metric)
+        hist_plugin.normalize_channels(f_hist)
+        hist_plugin.normalize_channels(w_hist)
 
-        return dists
+        return hist_plugin.compare_hist(f_hist, w_hist)
 
     def track_objects(self, f_id, frame, contour, hierarchy, wall, rectangle):
         """
@@ -233,44 +294,19 @@ class FrameObjektTracker:
         Not all tracked things move; Consider flashing lights or any localized, repetitive change. Tracking seizes!
         Objects can suddenly appear or appear to change *size* if the frame drags due to network latency. Back referencing frames needs a cache.
         """
+
         self.f_id = f_id
-        self.contour = contour
         self.contour_id = str(uuid.uuid4()).split('-')[0]
-        distances = self.get_histograms(frame, wall, rectangle, 'euclidean')            # array of euclidean distance to the previous image
 
-        self._ml = self.get_mean_location(self.contour)                                 # find the mean location of this target
+        self._ml = self.get_mean_location(contour)       # find the mean location of this target
 
-        for o in self.label_locations():                                                # go label this location as either NEW, PREV or INITIAL.
+        for o in self.label_locations(frame, wall, rectangle):  # go label this location as either NEW, PREV or INITIAL.
 
-            o.wall = wall
-            o.contour = self.contour                    # this is one of a group of sorted contours; it could be the first or the smallest. assume first, largest.
-            o.hierarchy = hierarchy                     # unused, will be i of an enumeration
-            o.rect = rectangle                          # NOW WE GET A RECT.
-            o.is_inside = is_inside(o.ml, o.rect)       # is the current ml inside the current rect? this is MY RECTANGLE, otherwise it is some other contour.
+            o.contour = contour                          # this is one of a group of sorted contours; it could be the first or the smallest. first is largest.
+            o.hierarchy = hierarchy                      # unused for now, will be i of an enumeration
 
-            off = self.l_delta_pcnt * o.md              # did o suddenly appear?
-            o.close = in_range(o.curr_dist, o.md, off)  # is the current distance within a range of the median of distances for the last thing with a tag?
-            print(f'\t\t\t\t\t\tis_inside: {o.is_inside} rect: {o.rect} close: {o.close}')
-            # there may not be a previous tag, frame or anything...
-            if o.prev_tag:
-                prev_wall = self.tracked.get(o.prev_tag).wall               # we have wall, thus a previous frame
-                X = self.make_grey_data(prev_wall, rectangle)
-                Y = self.make_grey_data(wall, rectangle)
-                self.set_frame_delta(X, Y)                                  # compare to current wall
-
-                o.fd = self._frame_delta                                    # delta of wall image to current f
-                self.fd_mean = np.mean(self._frame_deltas)                  # a float,
-                self.d_range = self.f_delta_pcnt * self.fd_mean             # percentage of px difference
-
-                # does this f match the previous f
-                # and thus the previous tag? make an
-                # evaluation based on delta between frames.
-                # if not in_range(o.fd, self.fd_mean, self.d_range):
-                #     # o.tag = f"{self.f_id}_{o.prev_tag.split('_')[1]}"
-                #     o.tag = o.create_tag(self.f_id)
-
-            else:
-                o.tag = o.create_tag(self.f_id)
+            if not o.prev_tag:
+                o.prev_tag = o.tag
 
             self.tracked[o.tag] = o
 
