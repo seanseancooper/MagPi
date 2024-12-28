0f95e7e461b8a573c2d10355cc4e984435d28fcd
seanseancooper <sean.darwin.cooper@gmail.com>
2024-12-27 18:47:40 -0700

    cleaned up formatting & comments.

--- src/cam/Showxating/lib/FrameObjekt.py
+++ src/cam/Showxating/lib/FrameObjekt.py
@@ -2,32 +2,33 @@ from datetime import datetime
 import numpy as np
 import uuid
 
-class FrameObjekt:
 
+class FrameObjekt:
 
     def __init__(self, f_id):
 
         # metadata
-        self.f_id = f_id                                    # frame id when created.
-        self.timestamp = datetime.now()                     # not used yet
-        self.tag = None                                     # [deprecate this in favor of using the f_id, portable] string unique identifier for this frame
-        self.isNew = True                                   # boolean is the id a NEW id or derived from f[-1]
-        self.skip = False                                   # [is this logical to have, useful or relevant?] should this frame be skipped in tagging f[+1]
+        self.f_id = f_id                                                # frame id when created.
+        self.timestamp = datetime.now()                                 # not used yet
+        self.tag = None                                                 # [deprecate this in favor of using the f_id, portable] string unique identifier for this frame
+        self.isNew = True                                               # boolean is the id a NEW id or derived from f[-1]
+        self.skip = False                                               # [is this logical to have, useful or relevant?] should this frame be skipped in tagging f[+1]
 
-        # frame data: features [remove unused things, decide when things are used]
+        # frame data:
+        # features [remove unused things, decide when things are used]
         self.contours = None                                            # [tuple of ndarray(n, 1, 2): object tracking] ALL contours in this frame
         self.hierarchy = None                                           # [ndarray  1, n, 4: unused] ordering of the contours in this frame  (UNUSED)
         self.prev_tag = ''                                              # [string: object tracking] tag of nearest FrameObjekt from the previous frame
-        self.contour_id = None                                            # [string: object tracking] id of source contour
+        self.contour_id = None                                          # [string: object tracking] id of source contour
         self.curr_dist = 0                                              # [int32: object tracking] euclidean_distance wrt previous mean x, y location
-        self.distances = np.zeros(shape=[1, 1], dtype=np.float64)     # [list([1,1]): object location] of previous euclidean_distances wrt previous mean x, y locations.
+        self.distances = np.zeros(shape=[1, 1], dtype=np.float64)       # [list([1,1]): object location] of previous euclidean_distances wrt previous mean x, y locations.
         self.fd = float()                                               # [float: reporting] euclidean_distance wrt previous frame analysis area
         self.fd_mean = float()
         self.delta_range = float()
         self.hist_delta = float()                                       # [float: reporting] histogram distance wrt previous frame analysis area
         self.rect = None                                                # [tuple {x, y, w, h}: object segmentation] bounding rects of contours in this frame
-        self.avg_loc = np.ndarray(shape=[2, ], dtype=np.int32)                # [tuple {x,y}: object segmentation] mean x, y location of *this* contour
-        self.dist_mean = float()                                               # mean of self.distances
+        self.avg_loc = np.ndarray(shape=[2, ], dtype=np.int32)          # [tuple {x,y}: object segmentation] mean x, y location of *this* contour
+        self.dist_mean = float()                                        # mean of self.distances
 
         self.wall = None                                                # [ndarray: container] image of processed area in this frame
 
@@ -36,11 +37,15 @@ class FrameObjekt:
         self.hist_pass = None
         self.wall_pass = None
 
-
     @staticmethod
     def create(f_id):
         o = FrameObjekt(f_id)
         return o
+
+    @staticmethod
+    def create_tag(f_id):
+        tag = f"{f_id}_{str(uuid.uuid4())}"
+        return tag
 
     def get(self):
         return {'f_id'          : self.f_id,
@@ -57,8 +62,3 @@ class FrameObjekt:
                 'hist_pass'     : self.hist_pass,
                 'wall_pass'     : self.wall_pass,
                 }
-
-    @staticmethod
-    def create_tag(f_id):
-        tag = f"{f_id}_{str(uuid.uuid4())}"
-        return tag
