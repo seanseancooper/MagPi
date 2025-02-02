19da977147ae7b29848093a538960fe86592bb0a
seanseancooper <sean.darwin.cooper@gmail.com>
2025-02-02 15:14:09 -0700

    new method 'write_trapped' for training caps.

--- src/cam/Showxating/lib/ImageWriter.py
+++ src/cam/Showxating/lib/ImageWriter.py
@@ -6,17 +6,16 @@ from src.config import CONFIG_PATH, readConfig
 
 class ImageWriter:
 
-    def __init__(self, writer_name: str):
+    def __init__(self, writer_name):
         self.writer_name = writer_name
-        self.IMAGE_OUT = None
         self.OUTFILE_NAME = None
         self.config = {}
 
     def write(self, writer_name: str, frame, f_id=None):
         readConfig('cam.json', self.config)
-        self.IMAGE_OUT = self.config['PLUGIN'].get('outfile_path', '../_out')
+        _out = self.config['PLUGIN'].get('outfile_path', '../_out')
         if writer_name:
-            PATH = os.path.join(os.getcwd(), self.IMAGE_OUT, self.writer_name)
+            PATH = os.path.join(os.getcwd(), _out, writer_name)
             if not os.path.exists(PATH):
                 os.makedirs(PATH)
 
@@ -28,6 +27,21 @@ class ImageWriter:
 
             self.OUTFILE_NAME = os.path.join(PATH, filename)
             cv.imwrite(self.OUTFILE_NAME, frame)
+
+        return True
+
+    def write_trapped(self, writer_name, _out, frame):
+        readConfig('cam.json', self.config)
+
+        if writer_name:
+            PATH = os.path.join(os.getcwd(), _out, writer_name)
+            if not os.path.exists(PATH):
+                os.makedirs(PATH)
+
+            NOW = datetime.now().strftime('%Y%m%d_%H%M%S')
+            filename = f"{NOW}_trapped.jpg"
+
+            cv.imwrite(os.path.join(PATH, filename), frame)
 
         return True
 
