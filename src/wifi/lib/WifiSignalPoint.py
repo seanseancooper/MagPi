409b031aceca5334a4c4c4dad0abeb2f3f1f05a4
seanseancooper <sean.darwin.cooper@gmail.com>
2025-03-11 23:17:41 -0600

    added worker_id field. This is populated after matching BSSID (wee WifiWorker: str(self.bssid).replace(':', '').lower())

--- src/wifi/lib/WifiSignalPoint.py
+++ src/wifi/lib/WifiSignalPoint.py
@@ -2,37 +2,23 @@ import uuid
 from datetime import datetime
 from src.lib.utils import format_time, format_delta
 
+
 class WifiSignalPoint:
     """ discrete class to encapsulate a signal captured at a point.
         A list of this type are assumed to be attributed to a specific bssid.
-        The id field serves to provide a unique representation of a specific
-        point.
+        The 'id' field serves to provide a unique representation of a specific
+        point. The 'worker_id' is derived from the BSSID of the AP that
+        produced it.
      """
 
-    def __init__(self, bssid, lon, lat, sgnl):
-        self._dt = datetime.now()
+    def __init__(self, worker_id, bssid, lon, lat, sgnl):
+        self._created = datetime.now()
         self._bssid = bssid
+        self._worker_id = worker_id
         self._id = uuid.uuid4()
         self._lon = lon
         self._lat = lat
         self._sgnl = sgnl
-
-    # def __repr__(self):
-    #     return "<Point x:{0},y:{1}>".format(self.x, self.y)
-    #
-    # implement addition (Adding 'Points' isn't natively supported)
-    # def __add__(self, other):
-    #     return Point(self.x + other.x, self.y + other.y)
-    #
-    # implement subtraction (Subtracting 'Points' isn't natively supported)
-    # def __sub__(self, other):
-    #     return Point(self.x - other.x, self.y - other.y)
-    #
-    # implement in-place addition  (in-place adding 'Points' isn't natively supported)
-    # def __iadd__(self, other):
-    #     self.x += other.x
-    #     self.y += other.y
-    #     return self
 
     def getLatLon(self):
         return self._lat, self._lon
@@ -42,8 +28,9 @@ class WifiSignalPoint:
 
     def get(self):
         return {
-            "datetime": format_time(self._dt, "%Y-%m-%d %H:%M:%S.%f"),
+            "created": format_time(self._created, "%Y-%m-%d %H:%M:%S.%f"),
             "id": str(self._id),
+            "worker_id": self._worker_id,
             "lon": self._lon,
             "lat": self._lat,
             "sgnl": self._sgnl
