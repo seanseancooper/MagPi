97d1962a92e315d5eeda999d9f9fe7c1657c9ee7
seanseancooper <sean.darwin.cooper@gmail.com>
2025-03-25 16:22:27 -0600

    uses latitude and longitude attribute of 'o'. this is geohashed here (tested, working). playing with  scaling numerical data. split numerical features. uses w_hist field for hashed histogram.

--- src/cam/Showxating/lib/FrameObjektEncoder.py
+++ src/cam/Showxating/lib/FrameObjektEncoder.py
@@ -1,30 +1,30 @@
 import threading
 
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 import numpy as np
 import geohash
-import cv2
+import cv2 as cv
 import hashlib
 
 
-class ObjektEncoder(threading.Thread):
+class FrameObjektEncoder(threading.Thread):
 
     def __init__(self, frame_obj):
         super().__init__()
         self.frame_obj = frame_obj
         self.hash_histogram = True
-        self.precision = 8
-        self.bins = 12
-        self.scaler = MinMaxScaler()
+        self.precision = 8                      # TOOD: make config
+        self.mm_scaler = MinMaxScaler()
+        self.std_scaler = StandardScaler()
 
     def encode_lat_lon(self, latitude, longitude):
         return geohash.encode(latitude, longitude, precision=self.precision)
 
-    def extract_color_histogram_from_image(self, image):
-        # images, channels, mask, histSize, ranges[, hist[, accumulate]]
-        histogram = cv2.calcHist([image], [0, 1, 2], None, [self.bins, self.bins, self.bins], [0, 256, 0, 256, 0, 256])
-        histogram = cv2.normalize(histogram, histogram).flatten()
-        return histogram.tolist()
+    # def extract_color_histogram_from_image(self, image):
+    #     # images, channels, mask, histSize, ranges[, hist[, accumulate]]
+    #     histogram = cv.calcHist([image], [0, 1, 2], None, [self.bins, self.bins, self.bins], [0, 256, 0, 256, 0, 256])
+    #     histogram = cv.normalize(histogram, histogram).flatten()
+    #     return histogram.tolist()
 
     def hash_color_histogram(self, histogram):
         histogram_str = ','.join(map(str, histogram))
@@ -36,46 +36,47 @@ class ObjektEncoder(threading.Thread):
             [
                 self.frame_obj.fd,
                 self.frame_obj.fd_mean,
-                # self.frame_obj.delta_range,
                 self.frame_obj.hist_delta,
+                self.frame_obj.dist_mean,
+            ]
+        ])
+
+        location_features = np.array([
+            [
                 self.frame_obj.rect[0],
                 self.frame_obj.rect[1],
                 self.frame_obj.rect[2],
                 self.frame_obj.rect[3],
                 self.frame_obj.avg_loc[0],
                 self.frame_obj.avg_loc[1],
-                self.frame_obj.dist_mean,
             ]
         ])
 
-        normalized_features = self.scaler.fit_transform(numerical_features)
+        norm_numerical_features = self.mm_scaler.fit_transform(numerical_features)
+        norm_location_features = self.mm_scaler.fit_transform(location_features)
+        # get this from gps or objekt?
         lat_lon_features = self.encode_lat_lon(self.frame_obj.lat, self.frame_obj.lon)
 
-        # encoded_data = []
-        # for features, obj, lat_lon in zip(normalized_features, self.frame_obj, lat_lon_features):
-        #     color_histogram = None
-        #     hashed_histogram = None
-        #
-        #     if obj.wall is not None:
-        #         color_histogram = self.extract_color_histogram_from_image(obj.wall)
-        #         if self.hash_histogram:
-        #             hashed_histogram = self.hash_color_histogram(color_histogram)
-        #
-        #     combined = list(features) + (color_histogram if color_histogram else []) + [hashed_histogram] + [lat_lon]
-        #     encoded_data.append(combined)
-        #
-        # return encoded_data
-
-        color_histogram = self.frame_obj.w_hist
         hashed_histogram = None
-
         if self.frame_obj.w_hist is not None:
+
+            histogram = np.array(self.frame_obj.w_hist)
+            normalized = cv.normalize(histogram, histogram)
+            flattened = normalized.flatten()
+            color_histogram = flattened.tolist()
+            hashed_histogram = None
+
             if self.hash_histogram:
                 hashed_histogram = self.hash_color_histogram(color_histogram)
 
-        encoded_data = list(normalized_features) + (color_histogram if color_histogram else []) + [hashed_histogram] + [lat_lon_features]
+        encoded_data = list(norm_numerical_features) + list(norm_location_features) + [lat_lon_features] + [hashed_histogram]
 
-        print(encoded_data)
+        # do something with encoded_data...
+        # OFFLINE PROCESSING FEATURES
+        # event list --> push events, labels and image refs to elastic
+        # vehicle id: available in a model?
+        # license plate reader: is this available in a model?
+        print(f'encoded data {self.frame_obj.f_id}: {encoded_data}')
         return encoded_data
 
 
