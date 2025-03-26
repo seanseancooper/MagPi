85c5a09d28448b73b8a1be938a569962a70d993c
seanseancooper <sean.darwin.cooper@gmail.com>
2025-03-26 14:49:53 -0600

    moved WifiWorkerParser into lib/net_utils; this is where I would expect to find these.

--- src/net/lib/net_utils.py
+++ src/net/lib/net_utils.py
@@ -65,3 +65,31 @@ def dict_to_frameobjekt(data):
     frame_obj.hist_pass = data['hist_pass']
     frame_obj.wall_pass = data['wall_pass']
     return frame_obj
+
+
+class WifiWorkerParser:
+
+    def __init__(self, data):
+        self.data = data
+        self.worker_data = {}
+        self.signal_data = {}
+        self.signal_test = {}
+        self._parse()
+
+    def _parse(self):
+        self.worker_data = self.data
+        self.signal_data = self.worker_data['signal_cache']
+        self.signal_test = self.worker_data['results']
+
+        self.worker_data.pop('signal_cache')    # remove the signal cache
+        self.worker_data.pop('results')         # remove the tests
+
+    def get_worker_data(self):
+        return self.worker_data
+
+    def get_signal_data(self):
+        return self.signal_data
+
+    def get_test_data(self):
+        return self.signal_test
+
