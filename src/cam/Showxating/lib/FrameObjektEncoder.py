14c7e535e5e62f388a4291b421c2695c0bcc883d
seanseancooper <sean.darwin.cooper@gmail.com>
2025-04-02 18:10:54 -0600

    implementing histograms (2 different methods) into encoder; the next step is to figure oout what to do with the image the histgram is made from.
    I think I will try passing this with zeroMQ, not quite there yet.

--- src/cam/Showxating/lib/FrameObjektEncoder.py
+++ src/cam/Showxating/lib/FrameObjektEncoder.py
@@ -5,7 +5,7 @@ import numpy as np
 import geohash
 import cv2 as cv
 import hashlib
-
+from src.cam.Showxating.ShowxatingHistogramPlugin import ShowxatingHistogramPlugin
 
 class FrameObjektEncoder(threading.Thread):
 
@@ -13,6 +13,7 @@ class FrameObjektEncoder(threading.Thread):
         super().__init__()
         self.frame_obj = frame_obj
         self.hash_histogram = True
+        self.bins = 32                          # get from config and configure
         self.precision = 8                      # TOOD: geohash precision, make config
         self.mm_scaler = MinMaxScaler()
         self.std_scaler = StandardScaler()
@@ -20,11 +21,27 @@ class FrameObjektEncoder(threading.Thread):
     def encode_lat_lon(self, latitude, longitude):
         return geohash.encode(latitude, longitude, precision=self.precision)
 
-    # def extract_color_histogram_from_image(self, image):
-    #     # images, channels, mask, histSize, ranges[, hist[, accumulate]]
-    #     histogram = cv.calcHist([image], [0, 1, 2], None, [self.bins, self.bins, self.bins], [0, 256, 0, 256, 0, 256])
-    #     histogram = cv.normalize(histogram, histogram).flatten()
-    #     return histogram.tolist()
+    def get_histogram(self, wall, rectangle):
+
+        hist_plugin = ShowxatingHistogramPlugin()
+        hist_plugin.plugin_name = 'histogramPlugin'
+        hist_plugin.get_config()
+        hist_plugin.f_id = self.frame_obj.f_id
+        hist_plugin._kz = (3, 3)                            # self.kernel_sz
+        hist_plugin.library = 'cv'                          # TODO: add to configurable
+        hist_plugin.compare_method = cv.HISTCMP_CORREL
+        hist_plugin.norm_type = cv.NORM_MINMAX
+
+        histogram = hist_plugin.get_histogram(wall, rectangle)
+        histogram = cv.normalize(histogram, histogram)
+        flattened = histogram.flatten()
+        return flattened.tolist()
+
+    def extract_color_histogram_from_image(self, image):
+        # images, channels, mask, histSize, ranges[, hist[, accumulate]]
+        histogram = cv.calcHist([image], [0, 1, 2], None, [self.bins, self.bins, self.bins], [0, 256, 0, 256, 0, 256])
+        histogram = cv.normalize(histogram, histogram).flatten()
+        return histogram.tolist()
 
     def hash_color_histogram(self, histogram):
         histogram_str = ','.join(map(str, histogram))
@@ -41,34 +58,20 @@ class FrameObjektEncoder(threading.Thread):
             ]
         ])
 
-        location_features = np.array([
-            [
-                int(self.frame_obj.rect[0]),
-                int(self.frame_obj.rect[1]),
-                int(self.frame_obj.rect[2]),
-                int(self.frame_obj.rect[3]),
-                int(self.frame_obj.avg_loc[0]),
-                int(self.frame_obj.avg_loc[1]),
-            ]
-        ])
+        _s = self.frame_obj.f_shape
+        rect_array = np.asarray([int(_s[1]), int(_s[0]), int(_s[1]), int(_s[0])])
+        loc_array = np.asarray([int(_s[1]), int(_s[0])])
 
-        _s  = self.frame_obj.f_shape
-        rect_shape = [int(_s[1]), int(_s[0]), int(_s[1]), int(_s[0])]
-        loc_shape = [int(_s[1]), int(_s[0])]
-        rect_array = np.asarray(rect_shape)
-        loc_array = np.asarray(loc_shape)
         encoded_rect = np.divide((np.asarray(self.frame_obj.rect)), rect_array)
         encoded_avg_loc = np.divide((np.asarray(self.frame_obj.avg_loc)), loc_array)
-        print(encoded_rect, encoded_avg_loc)
-
         norm_numerical_features = self.mm_scaler.fit_transform(numerical_features)
-        norm_location_features = self.mm_scaler.fit_transform(location_features)
         lat_lon_features = self.encode_lat_lon(self.frame_obj.lat, self.frame_obj.lon)
 
         hashed_histogram = None
-        if len(self.frame_obj.w_hist) > 0:
+        if self.frame_obj.w_hist:
 
-            histogram = np.array(self.frame_obj.w_hist)
+            # histogram = self.get_histogram(self.frame_obj.wall, self.frame_obj.rect)
+            histogram = self.extract_color_histogram_from_image(self.frame_obj.wall)
             normalized = cv.normalize(histogram, histogram)
             flattened = normalized.flatten()
             color_histogram = flattened.tolist()
@@ -76,7 +79,7 @@ class FrameObjektEncoder(threading.Thread):
             if self.hash_histogram:
                 hashed_histogram = self.hash_color_histogram(color_histogram)
 
-        encoded_data = list(norm_numerical_features) + list(norm_location_features) + [lat_lon_features] + [hashed_histogram]
+        encoded_data = list(norm_numerical_features) + list(encoded_rect) + list(encoded_avg_loc) + [lat_lon_features] + [hashed_histogram]
 
         # do something with encoded_data...
         # OFFLINE PROCESSING FEATURES
