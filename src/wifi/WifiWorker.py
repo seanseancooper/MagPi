7bc0b9afd2983e5f14d5fb01bdff1e98efba90fb
seanseancooper <sean.darwin.cooper@gmail.com>
2025-04-09 08:51:42 -0600

    added method to extract frequency features in update() method.

--- src/wifi/WifiWorker.py
+++ src/wifi/WifiWorker.py
@@ -35,6 +35,7 @@ class WifiWorker:
         self.return_all = False         # return all/any
         self.test_results = {}          # mapping of results
         self.cache_max = 0              # maximum number of SignalPoints displayed in logs
+        self._frequency_features = None
 
         self.DEBUG = False
 
@@ -54,12 +55,73 @@ class WifiWorker:
                 "is_mute"       : self.is_mute,
                 "tracked"       : self.tracked,
                 "signal_cache"  : [pt.get() for pt in self.scanner.signal_cache[self.bssid]][self.cache_max:],
+                "frequency_features"  : self._frequency_features,
                 "tests"         : [x for x in self.test_results]
         }
 
     # IDEA: probability that signal is mobile as
     #  opposed to stationary; features may be
     #  location, signal_cache, Vendor, Quality?
+
+    @staticmethod
+    def extract_signal_strength_features(past_signals, sampling_rate=1):
+
+        import numpy as np
+        from scipy.fftpack import fft
+        from python_speech_features import mfcc
+
+        MIN_LEN = 50
+
+        if len(past_signals) < 2:
+            return {
+                "dominant_freq": None,
+                "spectral_entropy": None,
+                "fft_magnitude": [0.0] * MIN_LEN,
+                "mfcc"         : [0.0] * 13
+            }
+
+        signal_values = np.array(past_signals)
+        N = len(signal_values)
+
+        if N < MIN_LEN:
+            signal_values = np.pad(signal_values, (0, MIN_LEN - N), 'constant')
+            N = MIN_LEN
+
+        fft_vals_full = np.abs(fft(signal_values))
+        fft_vals = fft_vals_full[:N // 2]
+
+        fft_vals_50 = np.zeros(MIN_LEN)
+        fft_clip = fft_vals[:MIN_LEN] if len(fft_vals) >= MIN_LEN else fft_vals
+        fft_vals_50[:len(fft_clip)] = fft_clip
+
+        freqs = np.fft.fftfreq(N, d=1 / sampling_rate)[:N // 2]
+        dominant_freq = freqs[np.argmax(fft_vals)]
+        normalized_fft = fft_vals / np.sum(fft_vals) if np.sum(fft_vals) > 0 else np.ones_like(fft_vals) / len(fft_vals)
+        spectral_entropy = -np.sum(normalized_fft * np.log2(normalized_fft + 1e-12))
+
+        try:
+            mfcc_features = mfcc(signal_values.reshape(-1, 1), samplerate=sampling_rate, numcep=13)
+            mfcc_mean = mfcc_features.mean(axis=0).tolist()
+        except Exception:
+            mfcc_mean = [0.0] * 13
+
+        # AUDIO FREQUENCY FEATURES:
+        # // ENOB: Effective number of bits
+
+        # DB (_sgnl): decibels
+        # SNR: Signal-to-noise ratio
+        # THD: Total harmonic distortion
+        # THD + N: Total harmonic distortion plus noise
+
+        # SFDR: Spurious free dynamic range
+        # SINAD: Signal-to-noise-and-distortion ratio
+
+        return {
+            "dominant_freq"   : float(dominant_freq),
+            "spectral_entropy": float(spectral_entropy),
+            "fft_magnitude"   : fft_vals_50.tolist(),
+            "mfcc"            : mfcc_mean
+        }
 
     def process_cell(self, cell):
         """ update static fields, tests"""
@@ -110,6 +172,9 @@ class WifiWorker:
         self.elapsed = self.updated - self.created
         self.tracked = self.bssid in self.scanner.tracked_signals
         self.scanner.make_signalpoint(self.id, self.bssid, int(sgnl.get('Signal', -99)))
+        self._frequency_features = self.extract_signal_strength_features(
+                [pt.getSgnl() for pt in self.scanner.signal_cache[self.bssid]]
+        )
 
     def match(self, cell):
         """ match BSSID, derive the 'id' and set mute status """
