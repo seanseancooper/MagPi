8d2b6461ca698b50825baff76dd733478182b70c
seanseancooper <sean.darwin.cooper@gmail.com>
2025-04-09 17:53:27 -0600

    refactored attributes and added rationale. __ is an attempt at making data immutable; I think it might work okay for the need:
    "This mechanism is designed to prevent accidental access or modification of these variables from outside the class."

--- src/lib/Signal.py
+++ src/lib/Signal.py
@@ -1,5 +1,3 @@
-import uuid
-from collections import defaultdict
 from datetime import datetime
 
 
@@ -7,34 +5,43 @@ class Signal:
     """
     Generic class to encapsulate a signal and  ad hoc map of attributes
     """
-    def __init__(self, data, signalpoint_id, sr=1):
+    def __init__(self, data, s_id, sr=1):
         self._created = datetime.now()  # when signal was created (differs from SignalPoint.created)
-        self._id = signalpoint_id       # id of asocciated Signalpoint
-        self._sr = sr                   # sampling rate of signal (see also 'attribute')
-        self._data = data               # [ndarray: container] data of T (is immutable)
+        self._id = s_id                 # id of associated SignalPoint object
+        self.__sr = sr                  # sampling rate of signal (immutable, see 'attributes')
+        self.__data = data              # [ndarray: container] data of T (immutable)
 
         self.attributes = {}            # ad hoc map of signal attributes
 
-        self.attributes.update({             # ad hoc map of signal attributes
-            "source": None,             # these are some ideas for defaults
-            "name": None,
-            "fs_path": None,
-            "id": self._id,
-            "signal_type": None,
-            "d_type": type(data),
-            "channels": None,
-            "sr": self._sr,
-            "tag": None
+        self.attributes.update({
+
+            # map of signal attributes (Who, What, When, Where, How, Why).
+            # defaults:
+
+            "source": None,             # who: source component, plugin or process
+            "signal_type": None,        # who: ???
+
+            "name": None,               # what: a human readable identifier
+            "d_type": type(self.__data),# what: 32bit, 64bit, complex, list?
+
+            "id": self._id,             # when: derived from creator SignalPoint type.
+
+            "fs_path": None,            # where: could be on filesystem
+
+            "channels": None,           # how: documentation of created value.
+            "sr": self.__sr,            # how: documentation of created value.
+
+            "tags": [None]              # why: a list of ad hoc tags for this Signal
         })
 
     def get_id(self):
         return self._id
 
     def get_sr(self):
-        return self._sr
+        return self.__sr
 
     def get_data(self):
-        return self._data
+        return self.__data
 
     def get_attributes(self):
         return self.attributes
