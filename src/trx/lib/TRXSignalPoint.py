65840c92b3df9e465f3b1d7986384497d8d33462
seanseancooper <sean.darwin.cooper@gmail.com>
2025-04-08 22:08:52 -0600

    updated field names. refactored type handling in TRXSignalPoint.

--- src/trx/lib/TRXSignalPoint.py
+++ src/trx/lib/TRXSignalPoint.py
@@ -18,7 +18,7 @@ class TRXSignalPoint(SignalPoint):
     This class encapsulates audio data for analysis and can switch between intermittent and continuous signal processing.
     """
 
-    def __init__(self, worker_id, lon, lat, sgnl, attributes, signal_data=None, audio_data=None, signal_type="discrete", sr=44100):
+    def __init__(self, worker_id, lon, lat, sgnl, attributes, signal_data=None, audio_data=None, signal_type="object", sr=44100):
         super().__init__(lon, lat, sgnl)
         self._worker_id = worker_id
         self._signal_type = signal_type         # continuous || intermittent
@@ -28,47 +28,47 @@ class TRXSignalPoint(SignalPoint):
         self.tracked = False
         self.is_mute = False
 
-        self._signal_data = signal_data         # Raw signal data (from SDR)?
-        self._audio_data = audio_data           # Raw audio data
+        self.t_attributes = {}                  # object (default): text from hardware, see aggregate(k, v)
+        self._signal_data = signal_data         # discrete: signal data (vector)
+        self._audio_data = audio_data           # continuous: audio data (numpy array)
         self._sampling_rate = sr                # need a default, a config, and ability to ad hoc change
-        self._text_data = defaultdict(dict)     # see aggregate
 
         self._frequency_features = None
 
-        #   ALPHATAG: name of system broadcasting
-        #   COMP_DATE: ??
-        #   COMP_TIME: ??
-        #   FREQ1: responding frequency?
-        #   FREQ2: initial freq?
-        # INFO
-        #   OBJECT_ID: system object broadcasting
-        # OTHER_TEXT
-        #   RID1
-        #   RID2
-        #   SCAN_DATE: replaced date
-        #   SCAN_TIME: replaced time
-        #   SITE: broadcast site
-        # SQ_MODE
-        # SQ_VALUE
-        #   SYSTEM: Broadcaster
-        #   TGID1
-        #   TGID2
-        #   TSYS_ID
-        #   TSYS_TYPE: broadcast system TYPE
-        #   TYPE: broadcast type
+        if attributes is not None:
+            #   ALPHATAG: name of system broadcasting
+            #   COMP_DATE: ??
+            #   COMP_TIME: ??
+            #   FREQ1: responding frequency?
+            #   FREQ2: initial freq?
+            # INFO
+            #   OBJECT_ID: system object broadcasting
+            # OTHER_TEXT
+            #   RID1
+            #   RID2
+            #   SCAN_DATE: replaced date
+            #   SCAN_TIME: replaced time
+            #   SITE: broadcast site
+            # SQ_MODE
+            # SQ_VALUE
+            #   SYSTEM: Broadcaster
+            #   TGID1
+            #   TGID2
+            #   TSYS_ID
+            #   TSYS_TYPE: broadcast system TYPE
+            #   TYPE: broadcast type
+            def aggregate(k, v):
+                self.t_attributes[k] = v
+            [aggregate(k, str(v)) for k, v in attributes.items()]
 
-        def aggregate(k,v):
-            self._text_data[k] = v
+        elif self._signal_type == "discrete" and self._signal_data is not None:
+            # an array of discrete floats or int; extract_signal_strength_features
+            self._frequency_features = self.extract_signal_strength_features(self._signal_data)
 
-        [aggregate(k, str(v)) for k, v in attributes.items()]
-
-        # If the signal is discrete, set sr and process as intermittent
-        if self._signal_type == "discrete" and self._text_data is not None:
-            self._frequency_features = self.extract_signal_strength_features(self._text_data)
-        elif self._signal_type == "continuous" and audio_data is not None:
-            # If continuous, set sr and use audio data and compute frequency features
-            arx = ARXSignalPoint(self._worker_id, self._lon, self._lat, self._sgnl, self._audio_data)
-            self._frequency_features = self.extract_audio_features(arx.get_audio_data(), arx.get_sampling_rate())
+        elif self._signal_type == "continuous" and self._audio_data is not None:
+            # set sr, make Signal. use audio data and extract_audio_features
+            signal = Signal(self._sampling_rate, self._audio_data)
+            self._frequency_features = self.extract_audio_features(signal.get_data(), signal.get_sr())
 
     def get_audio_data(self):
         """
@@ -94,17 +94,23 @@ class TRXSignalPoint(SignalPoint):
         """
         return {
             "created"           : format_time(self._created, "%Y-%m-%d %H:%M:%S.%f"),
+            "id"                : str(self._id),
             "lon"               : self._lon,
             "lat"               : self._lat,
             "sgnl"              : self._sgnl,
+            "updated"           : str(self.updated),
+            "elapsed"           : str(self.elapsed),
+            "is_mute"           : self.is_mute,
+            "tracked"           : self.tracked,
+
             "signal_type"       : self._signal_type,
-            "signal_data"       : self._signal_data,
+
+            "signal_data"       : [_ for _ in self._signal_data] if self._signal_data is not None else None,
             "audio_data"        : self._audio_data.tolist() if self._audio_data is not None else None,
-            "text_data"         : self._text_data,
+            "text_data"         : {k: v for k, v in self.t_attributes.items()},
+
             "frequency_features": self._frequency_features,
-
         }
-
 
     @staticmethod
     def extract_audio_features(audio_data, sampling_rate):
