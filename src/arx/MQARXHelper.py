b84be216611e2da996975f1b03dc80e133a3cc54
seanseancooper <sean.darwin.cooper@gmail.com>
2025-04-09 19:23:36 -0600

    stub helper for audio conversion. cleaned junk out of retriever..

--- src/arx/MQARXHelper.py
+++ src/arx/MQARXHelper.py
@@ -1,1 +1,18 @@
+from src.config import readConfig
+
+
+class MQARXHelper:
+    """ convert, transform and pass audio data """
+    def __init__(self):
+        super().__init__()
+
+        self.config = {}
+
+    def configure(self, config_file):
+        readConfig(config_file, self.config)
+
+    def run(self):
+        pass
+
+
 
