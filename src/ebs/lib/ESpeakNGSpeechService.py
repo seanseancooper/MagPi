9d012bbeeccb8cb19b6730d7b06d7b96ef43fbd5
seanseancooper <sean.darwin.cooper@gmail.com>
2025-04-10 00:57:19 -0600

    update non-essential components. UNTESTED.

--- src/ebs/lib/ESpeakNGSpeechService.py
+++ src/ebs/lib/ESpeakNGSpeechService.py
@@ -1,21 +1,37 @@
-from SpeechService import SpeechService
+import shutil
+import subprocess
 
-
-
+from src.ebs.lib.SpeechService import SpeechService
 
 
 class ESpeakNGSpeechService(SpeechService):
     """ ESpeakNG specific implementation of speech using local espeak command.
      This is targeted to GNU platforms and is a secondary focus due to
      anticipated disconnected environment """
+    def __init__(self):
+        super().__init__()
+        self.config = {}
 
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+    def configure(self):
+        super(ESpeakNGSpeechService, self).config()
 
+    def process_message(self):
+        if self.read_msg:
+            command = ['espeak', self.read_msg]
 
+            def runBlockingOSCommand(command: list):
+                try:
+                    command[0] = shutil.which(command[0])
+                    ps = subprocess.Popen(command)
+                    ps.wait()                               # mandatory: block until message completes
+                    return ps.pid
+                except OSError as e:
+                    print(f"[{__name__}]:couldn't create a process for \'{command}\': {e}")
+                return 0
 
+            pid = runBlockingOSCommand(command)
+            if pid > 0:
+                self.read_msg = None
 
 
 
--- src/ebs/lib/RemoteSpeechService.py
+++ src/ebs/lib/RemoteSpeechService.py
@@ -1,9 +1,5 @@
-from SpeechService import SpeechService
-
-
-
-
-
+from src.ebs.lib.SpeechService import SpeechService
+from google.cloud import texttospeech
 
 class RemoteSpeechService(SpeechService):
     """ Remote implementation of speech using local Google Speech,
@@ -12,44 +8,53 @@ class RemoteSpeechService(SpeechService):
 
       this will be implemented later in an vendor specific subclass.
       Not on critical path due to anticipated disconnected environment"""
+    def __init__(self):
+        super().__init__()
+        self.config = {}
+        self.speech_output = None
 
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+    def configure(self):
+        super(RemoteSpeechService, self).config()
+        self.speech_output = self.config['speech_output']
+
+    def process_message(self):
+        if self.read_msg:
+            self.synthesize_text(self.read_msg)
+            self.play_audio_file()
+
+    def synthesize_text(self, text):
+        """Synthesizes speech from the input string of text."""
+
+        client = texttospeech.TextToSpeechClient()
+
+        input_text = texttospeech.SynthesisInput(text=text)
+
+        # Note: the voice can also be specified by name.
+        # Names of voices can be retrieved with client.list_voices().
+        voice = texttospeech.VoiceSelectionParams(
+            language_code="en-US",
+            name="en-US-Standard-C",
+            ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
+        )
+
+        audio_config = texttospeech.AudioConfig(
+            audio_encoding=texttospeech.AudioEncoding.MP3
+        )
+
+        response = client.synthesize_speech(
+            request={"input": input_text, "voice": voice, "audio_config": audio_config}
+        )
+
+        # The response's audio_content is binary.
+        with open(self.speech_output, "wb") as out:
+            out.write(response.audio_content)
+            print('Audio content written to file "output.mp3"')
+
+    def play_audio_file(self):
+        # use ARX component to playback file
+        pass
 
 
-# def synthesize_text():
-#     """Synthesizes speech from the input string of text."""
-#     from google.cloud import texttospeech
-#
-#     text = "Hello there."
-#     client = texttospeech.TextToSpeechClient()
-#
-#     input_text = texttospeech.SynthesisInput(text=text)
-#
-#     # Note: the voice can also be specified by name.
-#     # Names of voices can be retrieved with client.list_voices().
-#     voice = texttospeech.VoiceSelectionParams(
-#         language_code="en-US",
-#         name="en-US-Standard-C",
-#         ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
-#     )
-#
-#     audio_config = texttospeech.AudioConfig(
-#         audio_encoding=texttospeech.AudioEncoding.MP3
-#     )
-#
-#     response = client.synthesize_speech(
-#         request={"input": input_text, "voice": voice, "audio_config": audio_config}
-#     )
-#
-#     # The response's audio_content is binary.
-#     with open("output.mp3", "wb") as out:
-#         out.write(response.audio_content)
-#         print('Audio content written to file "output.mp3"')
-#
-#
 
 
 
