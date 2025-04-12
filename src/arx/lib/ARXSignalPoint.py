ffaf1312bb45dbd34989f29ef740dea205910ca7
seanseancooper <sean.darwin.cooper@gmail.com>
2025-04-12 14:50:40 -0600

    removed audio_data and sr from constructor (won't have data). created methods to set/get sampling rate and refactored get_audio_data to put data in the current ARXSignalPoint. refactoed arxs_to_dict; wasn't getting data. removed bad .tolist() call in get()

--- src/arx/lib/ARXSignalPoint.py
+++ src/arx/lib/ARXSignalPoint.py
@@ -11,32 +11,35 @@ class ARXSignalPoint(SignalPoint):
     Class to encapsulate ARXRecorder recordings. This class holds a single audio data
     recording as a Signal() with a default sampling rate of 48Khz.
 
-    The clas computes frequency features using ARXEncoder.
+    The class computes frequency features using ARXEncoder.
     """
-    def __init__(self, worker_id, lon, lat, sgnl, audio_data=None, sr=48000):
+    def __init__(self, worker_id, lon, lat, sgnl):
         super().__init__(lon, lat, sgnl)
         self._worker_id = worker_id or 'ARXRecorder'
         self._signal_type = 'continuous'
         
-        self._audio_data = audio_data                   # potentially an array, a Signal() or LIST of type
-        self._sr = sr
+        self._audio_data = None                   # potentially an array, a Signal() or LIST of type
+        self._sr = None
         self._audio_frequency_features = None                 # a mapping of features, or LIST of mappings
 
         self._text_attributes = {}
         self._text_attributes.update({
             # map of signal attributes (Who, What, When, Where, How, Why).
             # defaults:
-            "source"            : None,           # who: source component, plugin or process
-            "signal_type"       : None,           # who: ???
-            "name"              : None,           # what: a human readable identifier
-            "d_type"            : type(self._audio_data),# what: 32bit, 64bit, complex, list?
-            "id"                : self._id,       # when: derived from creator SignalPoint type.
-            "fs_path"           : None,           # where: could be on filesystem
-            "channels"          : None,           # how: documentation of created value.
-            "shape"             : self._audio_data.shape(),           # how: documentation of created value.
-            "sr"                : self._sr,       # how: documentation of created value.
-            "tags"              : [None]          # why: a list of ad hoc tags for this Signal
+            "source"            : None,                     # who: source component, plugin or process
+            "signal_type"       : None,                     # who: ???
+            "name"              : None,                     # what: a human readable identifier
+            "d_type"            : str(type(self._audio_data)),   # what: 32bit, 64bit, complex, list?
+            "id"                : str(self._id),            # when: derived from creator SignalPoint type.
+            "fs_path"           : None,                     # where: could be on filesystem
+            "channels"          : None,                     # how: documentation of created value.
+            "shape"             : None,                     # how: documentation of created value.
+            "sr"                : self._sr,                 # how: documentation of created value.
+            "tags"              : [None]                    # why: a list of ad hoc tags for this Signal
         })
+
+    def get_worker_id(self):
+        return self._worker_id
 
     def get_text_attributes(self):
         return self._text_attributes
@@ -55,6 +58,9 @@ class ARXSignalPoint(SignalPoint):
     def get_signal_type(self):
         return self._signal_type
 
+    def set_sampling_rate(self, sr):
+        self._sr = sr
+
     def get_sampling_rate(self):
         return self._sr
 
@@ -62,30 +68,31 @@ class ARXSignalPoint(SignalPoint):
         return self._audio_data
 
     def set_audio_data(self, audio_data):
+
         def process_features(_data):
-            self._audio_data = Signal(_data, self._id, sr=self._sr)
             yield self.compute_audio_frequency_features()
 
-        if type(audio_data) is np.ndarray:
+        if isinstance(audio_data, np.ndarray):
             process_features(audio_data)
 
-        if type(audio_data) is list:
+        if isinstance(audio_data, list):
             self._audio_frequency_features = []
             [self._audio_frequency_features.append(process_features(_data)) for _data in audio_data]
+
+        self._audio_data = audio_data
 
     @staticmethod
     def arxs_to_dict(arxs):
         """ calculates on arxs """
-        audio_data = arxs.get_audio_data()
         return  {
-            "worker_id"                 : arxs.worker_id,
-            "lon"                       : arxs.lon,
-            "lat"                       : arxs.lat,
-            "sgnl"                      : arxs.sgnl,
-            "audio_data"                : audio_data,
+            "worker_id"                 : arxs._worker_id,
+            "lon"                       : arxs._lon,
+            "lat"                       : arxs._lat,
+            "sgnl"                      : arxs._sgnl,
+            "audio_data"                : arxs.get_audio_data(),
             "sr"                        : arxs.get_sampling_rate(),
             "signal_type"               : arxs.get_signal_type(),
-            "frequency_features"        : arxs.get_audio_frequency_features(),
+            "frequency_features"        : arxs.compute_audio_frequency_features(),
             "text_attributes"           : arxs.get_text_attributes(),
         }
 
@@ -93,12 +100,12 @@ class ARXSignalPoint(SignalPoint):
         """ return arxs *as is* """
         return {
             "created"           : format_time(self._created, "%Y-%m-%d %H:%M:%S.%f"),
-            "id"                : self._id,
+            "id"                : str(self._id),
             "worker_id"         : self._worker_id,
             "signal_type"       : self._signal_type,
             "lon"               : self._lon,
             "lat"               : self._lat,
-            "audio_data"        : self._audio_data.tolist() if self._audio_data is not None else None,
+            "audio_data"        : self._audio_data if self._audio_data is not None else None,
             "sr"                : self._sr,
             "frequency_features": self._audio_frequency_features,
             "text_attributes"   : self._text_attributes
@@ -113,9 +120,7 @@ class ARXSignalPoint(SignalPoint):
         sgnlpt  = ARXSignalPoint(d['worker_id'],
                                  d['lon'],
                                  d['lat'],
-                                 d['sgnl'],
-                                 audio_data=_audio_data,
-                                 sr=d.sr)
+                                 d['sgnl'])
 
         sgnlpt._worker_id = d.worker_id
         sgnlpt._signal_type = d['signal_type']
@@ -127,6 +132,7 @@ class ARXSignalPoint(SignalPoint):
 
     def compute_audio_frequency_features(self):
         # Compute frequency features for the audio data
+        print(f'compute_audio_frequency_features:{self._audio_data}')
         enc = ARXEncoder(self._audio_data, self._sr) # <-- now needs to  process multiple
         self._audio_frequency_features = enc.compute_audio_frequency_features()
-        yield self._audio_frequency_features
+        return self._audio_frequency_features
