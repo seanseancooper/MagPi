3f451a5a30dd359af5e99f9e0322d24c697964ef
seanseancooper <sean.darwin.cooper@gmail.com>
2025-04-17 10:30:08 -0600

    moved iteration sooner; will deal with old conditional next.

--- src/wifi/lib/WifiElasticMappingTransformer.py
+++ src/wifi/lib/WifiElasticMappingTransformer.py
@@ -53,42 +53,43 @@ class WifiElasticMappingTransformer:
         worker_index = f"worker_{worker_data['id']}"
         self.client.update(index=worker_index, id=worker_data['id'], doc=updt_worker_doc, ignore=400)
 
-    def process_workers(self, data):
+    def process_workers(self, item, mapTxfmr):
 
-        for item in data:
-            parser = WifiWorkerParser(item)
-            worker_data = parser.get_worker_data()
-            signal_data = parser.get_signal_data()
+        parser = mapTxfmr(item)
+        worker_data = parser.get_worker_data()
 
-            if worker_data['id'] in self._seen:
-                self.update_wifi_worker(worker_data, signal_data)
-            else:
-                # Insert worker data
-                worker_id = worker_data['id']
-                worker_index = f"worker_{worker_id}"
+        # this entire choice should be higher, earlier and remove signal_data and deps from this method
+        signal_data = parser.get_signal_data()  # why is signal data here in a worker method.
 
-                # transform created, updated representations to have a timezone
-                worker_created_time = datetime.strptime(worker_data['created'], self.config['DATETIME_FORMAT'])
-                worker_data['created'] = worker_created_time.astimezone(self.tz).isoformat()
+        if worker_data['id'] in self._seen:
+            self.update_wifi_worker(worker_data, signal_data)
+        else:
+            # Insert worker data
+            worker_id = worker_data['id']
+            worker_index = f"worker_{worker_id}"
 
-                worker_updated_time = datetime.strptime(worker_data['updated'], self.config['DATETIME_FORMAT'])
-                worker_data['updated'] = worker_updated_time.astimezone(self.tz).isoformat()
+            # transform created, updated representations to have a timezone
+            worker_created_time = datetime.strptime(worker_data['created'], self.config['DATETIME_FORMAT'])
+            worker_data['created'] = worker_created_time.astimezone(self.tz).isoformat()
 
-                worker_doc = {
-                    **worker_data,
-                    "Channel"  : int(worker_data['Channel']),
-                    "Frequency": int(worker_data['Frequency']),
-                    "Signal"   : int(worker_data['Signal']),
-                    "Quality"  : int(worker_data['Quality']),
-                    "signal_cache": [self.get_signalpoint(signal) for signal in signal_data]
-                }
+            worker_updated_time = datetime.strptime(worker_data['updated'], self.config['DATETIME_FORMAT'])
+            worker_data['updated'] = worker_updated_time.astimezone(self.tz).isoformat()
 
-                # Index worker document
-                self.client.index(index=worker_index, id=worker_id, document=worker_doc, ignore=400)
+            worker_doc = {
+                **worker_data,
+                "Channel"  : int(worker_data['Channel']),
+                "Frequency": int(worker_data['Frequency']),
+                "Signal"   : int(worker_data['Signal']),
+                "Quality"  : int(worker_data['Quality']),
+                "signal_cache": [self.get_signalpoint(signal) for signal in signal_data]
+            }
 
-                # Create signal index for worker
-                signals_index = f"{worker_data['id']}_signals"
-                self.client.indices.create(index=signals_index, body=self.signals_index_mapping, ignore=400)
+            # Index worker document
+            self.client.index(index=worker_index, id=worker_id, document=worker_doc, ignore=400)
+
+            # Create signal index for worker
+            signals_index = f"{worker_data['id']}_signals"
+            self.client.indices.create(index=signals_index, body=self.signals_index_mapping, ignore=400)
 
     def get_signalpoint(self, sgnl):
 
@@ -104,33 +105,33 @@ class WifiElasticMappingTransformer:
             "sgnl"     : int(sgnl["sgnl"])
         }
 
-    def process_signals(self, data):
+    def process_signals(self, item, mapTxfmr):
 
-        for item in data:
-            parser = WifiWorkerParser(item)
-            worker_data = parser.get_worker_data()
-            signal_data = parser.get_signal_data()
+        parser = mapTxfmr(item)
+        worker_data = parser.get_worker_data()
+        signal_data = parser.get_signal_data()
 
-            # Create signal index if needed
-            signals_index = f"{worker_data['id']}_signals"
+        # Create signal index if needed
+        signals_index = f"{worker_data['id']}_signals"
 
-            def _index(idx, sgnl):
-                """ index, signalpoint """
+        def _index(idx, sgnl):
+            """ index, signalpoint """
 
-                signal_doc = self.get_signalpoint(sgnl)
-                self.client.index(index=idx, id=sgnl["id"], document=signal_doc)
+            signal_doc = self.get_signalpoint(sgnl)
+            self.client.index(index=idx, id=sgnl["id"], document=signal_doc)
 
-            if worker_data['id'] in self._seen:
-                _index(signals_index, signal_data[-1])
-            else:
-                [_index(signals_index, signal) for signal in signal_data if signal_data]
-                self._seen.append(worker_data['id'])
+        if worker_data['id'] in self._seen:
+            _index(signals_index, signal_data[-1])
+        else:
+            [_index(signals_index, signal) for signal in signal_data if signal_data]
+            self._seen.append(worker_data['id'])
 
-    def push(self, data):
+    def push(self, data, mapTxfmr):
         """ method entrypoint for elastic indexing """
         try:
-            self.process_workers(data)
-            self.process_signals(data)
+            # this entire choice should be higher, earlier and remove signal_data and deps from this method
+            [self.process_workers(item, mapTxfmr) for item in data]
+            [self.process_signals(item, mapTxfmr) for item in data]
             print(f"Data pushed: {datetime.now().astimezone(self.tz).isoformat()}")
         except Exception as ex:
             print(f"Data push failed: {ex}")
