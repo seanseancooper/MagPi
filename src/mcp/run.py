9bc5e39646bb50cb154ae1322f1365ec751806d0
seanseancooper <sean.darwin.cooper@gmail.com>
2025-04-19 23:13:19 -0600

    added USE_OPENAI flag.

--- src/mcp/run.py
+++ src/mcp/run.py
@@ -2,7 +2,8 @@ from app.server import start_flask
 from pyngrok import ngrok
 
 # Flag to control whether ngrok is used or not
-USE_NGROK = False  # Set to True to enable ngrok
+USE_NGROK = False   # Set to True to enable ngrok
+USE_OPENAI = False  # Set to True to enable openai
 
 if __name__ == "__main__":
     if USE_NGROK:
@@ -17,6 +18,7 @@ if __name__ == "__main__":
 
     # Configure Flask app to reflect ngrok status
     app.config['USE_NGROK'] = USE_NGROK
+    app.config['USE_OPENAI'] = USE_OPENAI
 
     # Run the Flask app
     start_flask()
