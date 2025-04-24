e9b8c7cfec511395ee5bf95b63fe85a10b819a1f
seanseancooper <sean.darwin.cooper@gmail.com>
2025-04-24 16:46:27 -0600

    added import for retrievers

--- src/wifi/__init__.py
+++ src/wifi/__init__.py
@@ -1,7 +1,1 @@
-# from src.wifi.WifiController import WifiController
-# from src.lib.rest_server import RESTServer as RESTServer
-#
-# if __name__ == '__main__':
-#     w = WifiController()
-#     RESTServer(w.create_app()).run()
-#     w.run()
+from . import retrievers
