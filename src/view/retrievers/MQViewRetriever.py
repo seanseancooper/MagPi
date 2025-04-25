265a88a599299be8732f6d2f3cd1868db4c29ae6
seanseancooper <sean.darwin.cooper@gmail.com>
2025-04-24 23:23:47 -0600

    initial pass at MQ retrieval; working.

---
+++ src/view/retrievers/MQViewRetriever.py
@@ -0,0 +1,39 @@
+import threading
+
+from src.config import readConfig
+from src.net.rabbitMQ.RabbitMQAsyncConsumer import RabbitMQAsyncConsumer
+
+import logging
+
+logger_root = logging.getLogger('root')
+view_logger = logging.getLogger('view_logger')
+
+
+class MQViewRetriever(threading.Thread):
+    """ ViewController MQ data retriever """
+
+    def __init__(self):
+        super().__init__()
+        self.config = {}
+        self.consumer = None
+        self.DEBUG = False
+
+    def configure(self, config_file):
+        readConfig(config_file, self.config)
+
+        self.consumer = RabbitMQAsyncConsumer(self.config['AGGREGATOR_QUEUE'])
+        self.DEBUG = self.config.get('DEBUG')
+        self.start_consumer()
+
+    def start_consumer(self):
+        t = threading.Thread(target=self.consumer.run, daemon=True)
+        t.start()
+
+    def scan(self):
+        """ get data from MQ """
+        try:
+            return self.consumer.data or []
+        except Exception as e:
+            view_logger.error(f"[{__name__}]: Exception: {e}")
+
+
