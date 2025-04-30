06b35d78d11a90b6ad5d0daf7df6cbdd23e173e9
seanseancooper <sean.darwin.cooper@gmail.com>
2025-04-30 15:16:00 -0600

    comment

--- src/net/imageZMQ/ImageZMQAsyncProducer.py
+++ src/net/imageZMQ/ImageZMQAsyncProducer.py
@@ -8,17 +8,7 @@ from src.cam.lib.FrameObjekt import FrameObjekt
 logging.basicConfig(level=logging.INFO)
 
 class ImageZMQAsyncProducer:
-    def __init__(self):
-        self.sender = ImageSender(connect_to="tcp://127.0.0.1:5555")
-
-    def send_frame(self, frame_objekt: FrameObjekt):
-        metadata = {
-            "f_id": frame_objekt.f_id,
-            "created": frame_objekt.created.isoformat(),
-            "shape": frame_objekt.wall.shape
-        }
-        logging.info(f"Sending: {metadata}")
-        self.sender.send_image(json.dumps(metadata), frame_objekt.wall)
+    """ used to transport FrameObjekts and metadata """
 
 class CapsVid(threading.Thread):
 
