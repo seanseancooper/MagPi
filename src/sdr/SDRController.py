8ce59eea992523ba7f2b44f65f3ae7c482d47b18
seanseancooper <sean.darwin.cooper@gmail.com>
2025-04-29 22:40:37 -0600

    first cut at SDR cotrolleer and routes.
    These are modified versions of WIFI, I believe the interactions would largely the same. This isn't meant to be a fully adjustable SDR; it's designed to deal with sections of bandwith finding signals withing accoring to 'rules' for reception.

---
+++ src/sdr/SDRController.py
@@ -0,0 +1,47 @@
+import threading
+from flask import Flask
+from flask_cors import CORS, cross_origin
+import routes
+from src.lib.rest_server import RESTServer
+
+
+class SDRController(threading.Thread):
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def create_app():
+        """Create Flask application."""
+        app = Flask('sdr',
+                    instance_relative_config=True,
+                    subdomain_matching=True)
+
+        cors = CORS(app)
+        app.config['CORS_HEADERS'] = 'Content-Type'
+
+        with app.app_context():
+            if __name__ == '__main__':
+                app.config['SERVER_NAME'] = routes.scanner.config['SERVER_NAME']
+                app.config['DEBUG'] = routes.scanner.config['DEBUG']
+                app.register_blueprint(routes.sdr_bp)
+            return app
+
+    def run(self) -> None:
+        try:
+            import atexit
+
+            def stop():
+                routes.scanner.stop()
+
+            atexit.register(stop)
+
+            if __name__ == '__main__':
+                RESTServer(self.create_app()).run()
+            routes.scanner.run()
+        except KeyboardInterrupt:
+            pass
+
+
+if __name__ == '__main__':
+    SDRController().run()
---
+++ src/sdr/routes.py
@@ -0,0 +1,98 @@
+from flask import Blueprint, redirect, render_template, jsonify
+import logging
+
+from src.view.Scanner import Scanner
+
+scanner = Scanner()
+scanner.configure('sdr.json')
+
+sdr_bp = Blueprint(
+        'sdr_bp', __name__, subdomain='sdr',
+        template_folder=scanner.config['TEMPLATE_FOLDER'],
+        static_folder=scanner.config['STATIC_FOLDER'],
+        static_url_path='/static'
+)
+
+speech_logger = logging.getLogger('speech_logger')
+
+
+@sdr_bp.route('/', subdomain="sdr")
+def index():
+    return redirect("/scan", code=302)
+
+
+@sdr_bp.route('/scan', methods=['GET'], subdomain='sdr')
+def sdr_scan():
+    return jsonify(scanner.get_parsed_signals())
+
+
+@sdr_bp.route('/scan/<ident>', methods=['GET'], subdomain='sdr')
+def sdr_scan_ident(ident):
+    worker = scanner.module_tracker.get_worker(ident)
+    if worker:
+        return jsonify(worker.get())
+    return "", 404
+
+
+@sdr_bp.route('/scanner', methods=['GET'], subdomain='sdr')
+def sdr_scanner():
+    """ WIFI scanner UI pre viewcontainer. deprecated. """
+    return render_template("scanner.html.j2", scanner=scanner)
+
+
+@sdr_bp.route('/tracked', methods=['GET', 'POST'], subdomain='sdr')
+def sdr_tracked():
+    return jsonify(scanner.get_tracked_signals())
+
+
+@sdr_bp.route('/ghosts', methods=['GET', 'POST'], subdomain='sdr')
+def sdr_ghosts():
+    return jsonify(scanner.get_ghost_signals())
+
+
+@sdr_bp.route('/add/<ident>', methods=['POST'], subdomain='sdr')
+def add(ident):
+    if scanner.module_tracker.get_worker(ident).add(ident):
+        speech_logger.info(f'added')
+        return "OK", 200
+    return "", 404
+
+
+@sdr_bp.route('/mute/<ident>', methods=['POST'], subdomain='sdr')
+def mute(ident):
+    return str(scanner.module_tracker.get_worker(ident).mute()), 200
+
+
+@sdr_bp.route('/remove/<ident>', methods=['POST'], subdomain='sdr')
+def remove(ident):
+    if scanner.module_tracker.get_worker(ident).remove(ident):
+        speech_logger.info(f'removed')
+        return "OK", 200
+    return "", 404
+
+
+@sdr_bp.route('/config', methods=['GET'], subdomain='sdr')
+def sdr_config():
+    return jsonify(scanner.config)
+
+
+#  TODO: let other apps emit stats as well
+@sdr_bp.route('/stats', methods=['GET'], subdomain='sdr')
+def sdr_stats():
+    return jsonify(scanner.stats)
+
+
+@sdr_bp.route('/stop', methods=['POST'], subdomain='sdr')
+def sdr_stop():
+    return scanner.stop()
+
+
+@sdr_bp.route('/write', methods=['POST'], subdomain='sdr')
+def sdr_write():
+    from src.lib.utils import write_to_scanlist
+    if write_to_scanlist(scanner.config, scanner.get_tracked_signals()):
+        speech_logger.info(f'logged {len(scanner.get_tracked_signals())} items')
+        return "OK", 200
+    return "", 500
+
+
