e8fa4bd34a6072da7bfaf9147472bec0fc67f2d0
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-02 14:12:06 -0600

    now rehydrates FrameObjekt from metadata & data.

--- src/net/imageZMQ/ImageZMQAsyncConsumer.py
+++ src/net/imageZMQ/ImageZMQAsyncConsumer.py
@@ -3,33 +3,36 @@ import logging
 from imagezmq import ImageHub
 from src.cam.lib.FrameObjekt import FrameObjekt
 from datetime import datetime
-logging.basicConfig(level=logging.INFO)
+
+logger_root = logging.getLogger('logger_root')
 
 class ImageZMQAsyncConsumer:
-    """ used to transport FrameObjekts and metadata """
+    """ Transport FrameObjekt in the CAM module """
 
     def __init__(self, host, port):
-        self.hub = ImageHub(open_port=f'tcp://*:{port}')
+        self.hub = ImageHub(open_port=f'tcp://{host}:{port}')
 
     def receive_frame(self):
         while True:
-            metadata_json, frame = self.hub.recv_image()
-            metadata = json.loads(metadata_json)
-            frameobjekt = FrameObjekt.dict_to_frameobjekt(metadata)
+            metadata_json, f = self.hub.recv_image()
 
-            # # Start encoder thread to process frame
+            """ rehydrate FrameObjekt from metadata & data """
+            metadata = json.loads(metadata_json)
+            metadata['time_diff'] = (datetime.now() - metadata['created']).total_seconds()
+            frameobjekt = FrameObjekt.dict_to_frameobjekt(metadata)
+            frameobjekt.wall = f
+
+            # Start encoder thread to process FrameObjekt
             # from src.cam.lib.FrameObjektEncoder import FrameObjektEncoder
             # encoder = FrameObjektEncoder(frame_objekt)
             # encoder.start()
 
-            metadata['time_diff'] = (datetime.now() - frameobjekt.created).total_seconds()
-
-            print(f"Received {metadata}")
+            logger_root.info(f"Received {metadata}")
             self.hub.send_reply(b"OK")
 
 if __name__ == "__main__":
 
-    host = '127.0.0.1'
-    port = '5555'
-    consumer = ImageZMQAsyncConsumer(host, port)
+    t_host = 'localhost'
+    t_port = '5555'
+    consumer = ImageZMQAsyncConsumer(t_host, t_port)
     consumer.receive_frame()
