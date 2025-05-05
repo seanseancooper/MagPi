d964bf8f5ae813e2e9dbff7bf89defa5d6013ff7
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-04 19:28:38 -0600

    renamed WifiWorkerParser to WifiMappingParser; not just workerss, signals too.
    in ElasticMappingTransformer, this would be a Parser not a transformer.

--- src/wifi/lib/__init__.py
+++ src/wifi/lib/__init__.py
@@ -3,4 +3,4 @@ from . import ElasticMappingTransformer
 from . import iw_parse
 from . import wifi_utils
 from . import WifiSignalPoint
-from . import WifiWorkerParser
+from . import WifiMappingParser
