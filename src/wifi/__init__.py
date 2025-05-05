5758509367b61ca166fffd10fa9120947ffadb8d
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-04 23:08:14 -0600

    refactoring import structure for wifi; this has runtime imports and the retrievers/trackers ould not be found in the module tree.
    I want this to not be the case; I'll experiment with moving things around to see what works.

---
+++ src/wifi/__init__.py
@@ -0,0 +1,2 @@
+from src.wifi.retrievers.MacOSAirportWifiRetriever import MacOSAirportWifiRetriever     # runtime import by Scanner.
+from src.wifi.retrievers.LinuxIWListWifiRetriever import LinuxIWListWifiRetriever       # runtime import by Scanner.
