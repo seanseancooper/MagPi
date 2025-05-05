7f185e2451c7ca38a25fbb847b5e6cb25c9caeeb
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-04 20:38:34 -0600

    removing extra __init__ files; yagni more oft than not.

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,8 @@
 from . import arx
 from . import cam
-from .net import can
-from . import config
-from .map import gps
 from . import lib
 from . import map
 from . import net
 from . import sdr
-
 from . import view
 from . import wifi
--- src/wifi/lib/__init__.py
+++ src/wifi/lib/__init__.py
@@ -1,3 +1,3 @@
-from . import cat_scanlists
+
 from . import wifi_utils
 from . import WifiSignalPoint
