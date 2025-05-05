b600bbe436be6107cf442b9ca2d4e61998f5c5d5
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-04 18:24:25 -0600

    moved MAPAggregator to lib folder.

--- src/map/routes.py
+++ src/map/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, render_template, jsonify
 
-from src.map.MAPAggregator import MAPAggregator
+from src.map.lib.MAPAggregator import MAPAggregator
 mapAgg = MAPAggregator()
 # TODO: the config hasn't been read yet, how can these be configurable?
 non_config_files = ['arx.json', 'cam.json', 'ebs.json', 'gps.json', 'view.json']
