77e6171cbf5141891db826ca0349c99c708f47a9
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-09 22:00:18 -0600

    tyring to implement a check for ZMQ, but having trouble due to address being in use.
    I am doing this wrong. I don't want to load MQProvider too do the test; I want to know is MQ is working at a baseline. MQProvvider is specific to ARX and should stay that way -- I want to split the metadata out to rabbit.
    
    The plan is look @ ZMQ unit 'tests' as a model.

--- src/net/lib/net_utils.py
+++ src/net/lib/net_utils.py
@@ -60,19 +60,21 @@ def check_zmq_available():
     ZMQ_AVAILABLE = True
 
     try:
-        # start a ZMQ consumer/producer and test zmq
-        consumer = ZeroMQAsyncConsumer()
         producer = ZeroMQAsyncProducer()
+        arxs = producer.test()
+        try:
+            metadata = arxs.get_text_attributes()
+            data = arxs.get_audio_data()
 
-        import asyncio
-        asyncio.run(consumer.receive_data())
-        producer.send_data(metadata={"test": 1}, audiodata=None)
+            import asyncio
+            asyncio.run(producer.send_data(metadata, data))
 
-        while consumer.receive_data():
-            if not consumer.metadata['test']:
-                ZMQ_AVAILABLE = False
-                net_logger.debug(f'NET::ZMQ not available: healthcheck failed.')
-            return ZMQ_AVAILABLE
+            # loop = asyncio.get_event_loop()
+            # loop.run_until_complete(producer.send_data(metadata, data))
+
+        except Exception:
+            ZMQ_AVAILABLE = False
+            net_logger.debug(f'NET::ZMQ not available: healthcheck failed.')
 
     except Exception as e:
         ZMQ_AVAILABLE = False
@@ -87,4 +89,10 @@ def check_imq_available():
         return check_zmq_available()
     except Exception as e:
         net_logger.debug(f'Error: NET::IMQ is not available: {e}')
+
+if __name__ == '__main__':
+    print(check_zmq_available())
+
+    # consumer = ZeroMQAsyncConsumer()
+    # consumer.run()
 
