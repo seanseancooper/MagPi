46be78c82f16af1dce23ce3af4e21703ac86e45b
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-11 14:13:17 -0600

    made the decision to remove RabbitMQ.
    My ideas are good, but I'm doing this wrong. The way this is structured now is complicated because I wanted to try some things out. I've since decided which flavor is of most benefit, least overhead and doesn't need a lot of extra had-holding to bee actionable. ZeroMQ fits the bill; it'ss lightweight and flexes well; RabbitMq needs a server startup and while it's entirely possible to use it -- needs a lot more management than the others.
    
    Did I mention that ZeroMQ & ImageMQ are related? ImageMQ uses ZeroMQ under the hood -- If I standardize on Zero for everything I can devvote ImageMQ to CAM, further simplifying things.
    
    I believe I made these experiments in a way that splits data from metadata and passes it over the apropos wire. This CAMMQProvider has removed RMQ. There is an issue causing the connection to block, but that's not a priority right now.

--- src/cam/mq/CAMMQProvider.py
+++ src/cam/mq/CAMMQProvider.py
@@ -1,8 +1,6 @@
 import threading
 
 from src.config import readConfig
-from src.net.rabbitMQ.RabbitMQProducer import RabbitMQProducer
-from src.net.lib.net_utils import check_rmq_available
 from src.net.imageZMQ.ImageZMQAsyncProducer import ImageZMQAsyncProducer
 
 import logging
@@ -15,7 +13,6 @@ class CAMMQProvider(threading.Thread):
     def __init__(self):
         super().__init__()
         self.config = {}
-        self.rmq = None
         self.imq = None
         self.imq_host = None
         self.imq_port = None
@@ -25,21 +22,12 @@ class CAMMQProvider(threading.Thread):
         readConfig(config_file, self.config)
         self.DEBUG = self.config.get('DEBUG')
 
-        _, RMQ_AVAIL = check_rmq_available(self.config['MODULE'])
-
-        if RMQ_AVAIL:
-            self.rmq = RabbitMQProducer(self.config['RABBITMQ_QUEUE'])
-
         self.imq_host = self.config.get('IMQ_HOST')
         self.imq_port = self.config.get('IMQ_PORT')
         self.imq = ImageZMQAsyncProducer(self.imq_host, self.imq_port)
 
     def send_frame(self, frame):        # metadata & image arrays via ImageZMQ
         self.imq.send_frame(frame)
-
-    def send_message(self, message):    # 'fields' via RabbitMQ
-        if self.rmq:
-            self.rmq.publish_message(message)
 
     def send_frameobjekt(self, frameobjekt):
         """ decompose a FrameObjekt into message, metadata and data """
@@ -50,8 +38,7 @@ class CAMMQProvider(threading.Thread):
             data = frameobjekt.wall                 # wall
             frame = metadata, data
 
-            self.send_frame(frame)      # metadata, arrays
-            self.send_message(message)  # fields & values
+            self.send_frame(frame)
 
         except Exception as e:
             print(f'Exception: {e}')
