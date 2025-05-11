1468a6061232f9fae940582f135518459330a6a2
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-11 13:52:26 -0600

    made the decision to remove RabbitMQ.
    My ideas are good, but I'm doing this wrong. The way this is structured now is complicated because I wanted to try some things out. I've since decided which flavor is of most benefit, least overhead and doesn't need a lot of extra had-holding to bee actionable. ZeroMQ fits the bill; it'ss lightweight and flexes well; RabbitMq needs a server startup and while it's entirely possible to use it -- needs a lot more management than the others.
    
    Did I mention that ZeroMQ & ImageMQ are related? ImageMQ uses ZeroMQ under the hood -- If I standardize on Zero for everything I can devvote ImageMQ to CAM, further simplifying things.
    
    I believe I made these experiments in a way that splits data from metadata and passes it over the apropos wire. This ARXMQProvider has removed RMQ and still works.

--- src/arx/mq/ARXMQProvider.py
+++ src/arx/mq/ARXMQProvider.py
@@ -1,10 +1,6 @@
-import json
 import threading
 import asyncio
 from src.config import readConfig
-from src.net.lib.net_utils import check_rmq_available
-from src.net.rabbitMQ.RabbitMQProducer import RabbitMQProducer
-from src.net.rabbitMQ.RabbitMQAsyncConsumer import RabbitMQAsyncConsumer
 from src.net.zeroMQ.ZeroMQAsyncProducer import ZeroMQAsyncProducer
 from src.net.zeroMQ.ZeroMQAsyncConsumer import ZeroMQAsyncConsumer
 
@@ -18,25 +14,17 @@ class ARXMQProvider(threading.Thread):
     def __init__(self):
         super().__init__()
         self.config = {}
-        self.rmq = None
         self.zmq = ZeroMQAsyncProducer()
         self.DEBUG = False
 
     def configure(self, config_file):
         readConfig(config_file, self.config)
         self.DEBUG = self.config.get('DEBUG')
-        _, RMQ_AVAIL = check_rmq_available(self.config['MODULE'])
-
-        if RMQ_AVAIL:
-            self.rmq = RabbitMQProducer(self.config['ARX_QUEUE'])
 
     def send_frame(self, frame):
         metadata, data = frame
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.zmq.send_data(metadata, data))
-
-    def send_message(self, message):
-        self.rmq.publish_message(message)
 
     def send_sgnlpt(self, arxs):
         try:
@@ -49,10 +37,6 @@ class ARXMQProvider(threading.Thread):
                 # print(f'sending zmq')
                 self.send_frame(frame)
 
-            if self.rmq:
-                # print(f'sending rmq')
-                self.send_message(json.dumps(text_attributes).encode('utf-8'))
-
         except Exception as e:
             print(f'{e}')
 
@@ -61,17 +45,12 @@ class ARXMQConsumer(threading.Thread):
     def __init__(self):
         super().__init__()
         self.config = {}
-        self.rmq = None
         self.zmq = ZeroMQAsyncConsumer()
         self.DEBUG = False
 
     def configure(self, config_file):
         readConfig(config_file, self.config)
         self.DEBUG = self.config.get('DEBUG')
-        _, RMQ_AVAIL = check_rmq_available(self.config['MODULE'])
-
-        if RMQ_AVAIL:
-            self.rmq = RabbitMQAsyncConsumer(self.config['ARX_QUEUE'])
 
     def get_data(self):
         return self.zmq.get_data()
@@ -83,13 +62,8 @@ class ARXMQConsumer(threading.Thread):
         frame = self.get_metadata(), self.get_data(),
         return frame
 
-    def get_message(self):
-        return self.rmq.data if self.rmq else None
-
     def consume(self):
         self.consume_zmq()
-        if self.rmq:
-            self.consume_rmq()
 
     def consume_zmq(self):
         try:
@@ -97,10 +71,3 @@ class ARXMQConsumer(threading.Thread):
             self.zmq.receive_data()
         except Exception as e: # don't trap here
             print(f'NET::ZMQ Error {e}')
-
-    def consume_rmq(self):
-        try:
-            t = threading.Thread(target=self.rmq.run)
-            t.start()
-        except Exception as e: # don't trap here
-            print(f'NET::RMQ Error {e}')
