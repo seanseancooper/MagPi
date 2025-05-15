c3587d864d883ab58f8ebcc326da623551b65ec5
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-15 15:37:12 -0600

    vite hostname and port are now configurable for MAP.

--- src/map/src/vite.config.js
+++ src/map/src/vite.config.js
@@ -1,5 +1,10 @@
 //import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import map_config from '../../config/map.json' assert { type: 'json' };
+
+// read config
+const map_host = map_config.MAP.find(item => item.MAP_HOST)['MAP_HOST'];
+const map_port = map_config.MAP.find(item => item.MAP_PORT)['MAP_PORT'];
 
 export default {
     root: '../src',
@@ -9,6 +14,10 @@ export default {
     build: {
         outDir: '../static',
         emptyOutDir: true, // necessary
+    },
+    server: {
+        host: map_host,
+        port: map_port,
     },
 }
 
