ee498c6f1698a9772a6ff9bb1a8ca3fe1943907f
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-18 13:13:04 -0600

    added TextFileTextRetriever as runtime import.

--- src/lib/__init__.py
+++ src/lib/__init__.py
@@ -1,1 +1,2 @@
 from src.lib.Tracker import Tracker     # runtime import by Scanner. remove this
+from src.lib.TextFileTextRetriever import TextFileTextRetriever     # runtime import by Scanner. remove this
