12879c945ce45b16eee5289900fa689d7633c300
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-27 23:13:51 -0600

    renamed methood.

--- src/lib/Worker.py
+++ src/lib/Worker.py
@@ -268,4 +268,5 @@ class Worker:
     def run(self):
         # this is long-standing ugly; I should not be brute-forcing matches
         # across parsed_cells to know what cell to operate on.
-        [self.match(cell) for cell in self.tracker.parsed_cells]
+        
+        [self.process_cell(cell) for cell in self.tracker.parsed_cells]
