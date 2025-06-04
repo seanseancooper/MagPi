0626709add5d33aebb31746ee6f6562d05015615
seanseancooper <sean.darwin.cooper@gmail.com>
2025-06-03 20:21:35 -0600

    refactored to seek times in the block so we can extract discrete signals.

--- src/sdr/lib/IQFileReader.py
+++ src/sdr/lib/IQFileReader.py
@@ -23,5 +23,12 @@ class IQFileReader:
                 continue
             return data
 
+    def seek_time(self, start_time, sample_rate):
+        byte_offset = int(start_time * sample_rate) * np.dtype(np.complex64).itemsize
+        self.file.seek(byte_offset, os.SEEK_SET)
+
+    def read_range(self, sample_count):
+        return np.fromfile(self.file, dtype=np.complex64, count=sample_count)
+
     def close(self):
         self.file.close()
