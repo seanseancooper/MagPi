11cc6d401e6cb93713b9064c59550e69938f222f
seanseancooper <sean.darwin.cooper@gmail.com>
2025-06-24 12:34:22 -0600

    setting this to a usable setting.

--- src/sdr/static/js/make_spectrogram.js
+++ src/sdr/static/js/make_spectrogram.js
@@ -110,7 +110,7 @@ let latestPeakData = new Uint8Array(1024);                  // Hold latest peak 
 const signalMetadataMap = new Map();                        // key: highlight ID or position, value: metadata
 
 let bgcolor = "#000099";
-let normBounds = { minDb: -100, maxDb: 100 };
+let normBounds = { minDb: -5, maxDb: 100 };
 let highlights = [
       { min_sel: 462, max_sel: 565, alpha: 0.4, color: "green" } // 1k bandwidth
 ];
