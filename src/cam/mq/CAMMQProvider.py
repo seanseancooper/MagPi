8c0ebcc1a738cccb2d6ba9957e135d0384943d90
seanseancooper <sean.darwin.cooper@gmail.com>
2025-06-25 17:34:14 -0600

    ImageZMQAsyncConsumer breaks this; not criticcall right now.

--- src/cam/mq/CAMMQProvider.py
+++ src/cam/mq/CAMMQProvider.py
@@ -2,6 +2,7 @@ import threading
 
 from src.config import readConfig
 from src.net.imageZMQ.ImageZMQAsyncProducer import ImageZMQAsyncProducer
+from src.net.imageZMQ.ImageZMQAsyncConsumer import ImageZMQAsyncConsumer
 
 import logging
 
@@ -13,16 +14,18 @@ class CAMMQProvider(threading.Thread):
     def __init__(self):
         super().__init__()
         self.config = {}
-        self.imq = None
+        self.producer = None
+        # self.consumer = None
         self.DEBUG = False
 
     def configure(self, config_file):
         readConfig(config_file, self.config)
         self.DEBUG = self.config.get('DEBUG')
-        self.imq = ImageZMQAsyncProducer(self.config.get('IMQ_HOST'),self.config.get('IMQ_PORT'))
+        self.producer = ImageZMQAsyncProducer(self.config.get('IMQ_HOST'),self.config.get('IMQ_PORT'))
+        # self.consumer = ImageZMQAsyncConsumer(self.config.get('IMQ_HOST'),self.config.get('IMQ_PORT'))
 
     def send_frame(self, frame):        # metadata & image arrays via ImageZMQ
-        self.imq.send_frame(frame)
+        self.producer.send_frame(frame)
 
     def send_frameobjekt(self, frameobjekt):
         """ decompose a FrameObjekt into message, metadata and data """
@@ -33,7 +36,7 @@ class CAMMQProvider(threading.Thread):
             data = frameobjekt.wall                 # wall
             frame = metadata, data
 
-            self.send_frame(frame)
+            # self.send_frame(frame)
 
         except Exception as e:
             print(f'Exception: {e}')
